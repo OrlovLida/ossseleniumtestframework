c2ded7f33620aebc5916b4bcc40470768c386b24
Gabriela Kasza <gabriela.kasza@comarch.com>
2020-06-16 11:03:27 +0200

    ER OSSWEB-7309 # Support for old Tabs added

---
+++ src/main/java/com/oss/framework/widgets/tabswidget/TabsInterface.java
@@ -0,0 +1,16 @@
+/* @(#) $$Id$$
+ *
+ * Copyright (c) 2000-2020 Comarch SA All Rights Reserved. Any usage,
+ * duplication or redistribution of this software is allowed only according to
+ * separate agreement prepared in written between Comarch and authorized party.
+ */
+package com.oss.framework.widgets.tabswidget;
+
+/**
+ * @author Gabriela Kasza
+ */
+public interface TabsInterface {
+   void selectTabByLabel(String tabLabel);
+   void callActionByLabel(String actionLabel);
+
+}
