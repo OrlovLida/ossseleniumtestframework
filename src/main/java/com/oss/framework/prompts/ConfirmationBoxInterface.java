3e81cb1de08c24fef3ef487b693ac8cfabe43a53
Gabriela Kasza <gabriela.kasza@comarch.com>
2020-06-22 09:19:30 +0200

    ER OSSWEB-7330 # Support for confirmation box added

---
+++ src/main/java/com/oss/framework/prompts/ConfirmationBoxInterface.java
@@ -0,0 +1,15 @@
+/* @(#) $$Id$$
+ *
+ * Copyright (c) 2000-2020 Comarch SA All Rights Reserved. Any usage,
+ * duplication or redistribution of this software is allowed only according to
+ * separate agreement prepared in written between Comarch and authorized party.
+ */
+package com.oss.framework.prompts;
+
+/**
+ * @author Gabriela Kasza
+ */
+public interface ConfirmationBoxInterface {
+    void clickButtonByLabel(String label);
+    String getMessage();
+}
