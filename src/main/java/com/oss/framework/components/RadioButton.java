a64181659e67f685a649074d802d4ebcec0424be
Gabriela Kasza <gabriela.kasza@comarch.com>
2020-07-28 11:18:38 +0200

    ER OSSWEB-8043 #  Add support for Radio Buttons

--- src/main/java/com/oss/framework/components/RadioButton.java
+++ src/main/java/com/oss/framework/components/RadioButton.java
@@ -7,11 +7,15 @@
 package com.oss.framework.components;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.google.common.collect.Lists;
 import com.oss.framework.data.Data;
 import com.oss.framework.utils.DelayUtils;
 
@@ -25,30 +29,35 @@ public class RadioButton extends Input {
 
     @Override
     public void setValue(Data value) {
-        setRadioButton(value);
-
-
+        String valueToSet = value.getStringValue();
+        DelayUtils.waitForNestedElements(webDriverWait,webElement,"//label[text()='" + valueToSet + "']");
+        WebElement radioButton = this.webElement.findElement(By.xpath("//label[text()='" + valueToSet + "']"));
+        radioButton.click();
     }
 
     @Override
     public void setValueContains(Data value) {
+        String valueToSet = value.getStringValue();
+        DelayUtils.waitForNestedElements(webDriverWait,webElement,"//label[contains(text(),'"+ valueToSet +"')]");
+        WebElement radioButton = this.webElement.findElement(By.xpath("//label[contains(text(),'" + valueToSet + "')]"));
+        radioButton.click();
 
     }
 
     @Override
     public Data getValue() {
-        return null;
+        List<String> names = new ArrayList<String>();
+        DelayUtils.waitByElement(webDriverWait,this.webElement);
+        List<WebElement> radioButtons = this.webElement.findElements(By.className("radio"));
+        for (WebElement radioButton:radioButtons) {
+            names.add(radioButton.getText());
+        }
+        return Data.createMultiData(names);
     }
 
     @Override
     public void clear() {
-
+        throw new RuntimeException("Method not implemented for the Radio Button");
     }
-    private void setRadioButton(Data value){
-        String valueToSet = value.getStringValue();
-        DelayUtils.waitByXPath(webDriverWait,"//label[contains(text(),'"+ valueToSet +"')]");
-        WebElement radioButton = driver.findElement(By.xpath("//label[contains(text(),'" + valueToSet + "')]"));
-        radioButton.click();
 
-    }
 }
