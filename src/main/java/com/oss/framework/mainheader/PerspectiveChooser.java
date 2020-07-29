89cd396dd06a8c2bc107411933ba861b67946f25
Gabriela Kasza <gabriela.kasza@comarch.com>
2020-07-29 10:43:32 +0200

    ER OSSWEB-7631 #  Data chooser, perspective withRemoved and withoutRemoved added

--- src/main/java/com/oss/framework/mainheader/PerspectiveChooser.java
+++ src/main/java/com/oss/framework/mainheader/PerspectiveChooser.java
@@ -6,12 +6,16 @@
  */
 package com.oss.framework.mainheader;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.oss.framework.components.Input;
+import com.oss.framework.components.RadioButton;
+import com.oss.framework.data.Data;
 import com.oss.framework.utils.DelayUtils;
 import com.oss.framework.widgets.Wizard;
 
@@ -26,6 +30,8 @@ public class PerspectiveChooser {
     private static String LIVE ="Live";
     private static String NETWORK="Network";
     private static String PLAN="Plan";
+    private static String WITH_REMOVE="With removed";
+    private static String WITHOUT_REMOVED = "Without removed";
 
 
     public static PerspectiveChooser create (WebDriver driver, WebDriverWait wait){return new PerspectiveChooser(driver, wait);}
@@ -49,13 +55,26 @@ public class PerspectiveChooser {
         Input input = planChooser.getComponent("searchBoxId", Input.ComponentType.SEARCH_FIELD);
         input.setSingleStringValue(processCodeOrName);
         planChooser.clickChange();
+        planChooser.waitToClose();
         wait.until(url -> driver.getCurrentUrl().contains("PLAN"));
     }
     public void setPlanDatePerspective (String date){
         setPerspective(PLAN);
         Wizard dataChooser = createWizard(driver, wait);
-        //TODO add radiobutton Class
+        Input radioButtons = dataChooser.getComponent("radioGroupId", Input.ComponentType.RADIO_BUTTON);
+        radioButtons.setSingleStringValue("Date");
+        Input selectDate = dataChooser.getComponent("dateFieldId", Input.ComponentType.DATE);
+        selectDate.setSingleStringValue(date);
+        dataChooser.clickChange();
         wait.until(url -> driver.getCurrentUrl().contains(date));
+    }
+    public void setWithRemove(){
+        setPerspective(WITH_REMOVE);
+        wait.until(url ->driver.getCurrentUrl().contains("withRemoved=true"));
+    }
+    public void setWithoutRemoved(){
+        setPerspective(WITHOUT_REMOVED);
+        wait.until(url ->driver.getCurrentUrl().contains("withRemoved=true"));
     }
      private void setPerspective(String perspective){
          MainHeader toolbar = MainHeader.create(driver, wait);
