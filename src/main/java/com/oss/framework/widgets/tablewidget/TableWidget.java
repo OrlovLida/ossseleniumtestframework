5aa8e0da8f787040b15cd59a49c2ec7852d297c0
Milena.Mietkiewicz <milena.mietkiewicz@comarch.com>
2020-08-27 11:22:48 +0000

    Resolve OSSWEB-8362

--- src/main/java/com/oss/framework/widgets/tablewidget/TableWidget.java
+++ src/main/java/com/oss/framework/widgets/tablewidget/TableWidget.java
@@ -334,6 +334,11 @@ public class TableWidget extends Widget implements TableInterface {
 
     }
 
+    @Override
+    public String getEmptyTable(String windowClass) {
+        return null;
+    }
+
     public void typeIntoSearch(String text){ getSearchInput().sendKeys(text); }
 
     public void scrollHorizontally(int offset){
