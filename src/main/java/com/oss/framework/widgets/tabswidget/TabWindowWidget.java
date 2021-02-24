e6973fe91368ed238795e2dcce8004b90b5a8c77
Gabriela Kasza <gabriela.kasza@comarch.com>
2021-02-24 15:46:39 +0100

    ER OSSWEB-11200 # checking if More exist for Old Tabs

--- src/main/java/com/oss/framework/widgets/tabswidget/TabWindowWidget.java
+++ src/main/java/com/oss/framework/widgets/tabswidget/TabWindowWidget.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.oss.framework.components.contextactions.ActionsContainer;
@@ -14,79 +15,103 @@ import com.oss.framework.utils.CSSUtils;
 import com.oss.framework.utils.DelayUtils;
 
 public class TabWindowWidget implements TabsInterface {
+    
+    private static final String DROPDOWN_TAB = ".//div[@class= 'dropdown']";
+    private static final String OSS_WINDOW_TAB = "//div[@class='OssWindow tabWindow']";
+    private static final String ALL_TABS = "//div[@class='tabs']";
+    
     private final WebDriver driver;
     private final WebDriverWait wait;
     private final WebElement tabs;
-
+    
     public static TabsInterface create(WebDriver driver, WebDriverWait wait) {
         DelayUtils.sleep(500);
-        DelayUtils.waitByXPath(wait, "//div[@class='OssWindow tabWindow']");
-        WebElement widget = driver.findElement(By.xpath("//div[@class='OssWindow tabWindow']"));
+        DelayUtils.waitByXPath(wait, OSS_WINDOW_TAB);
+        WebElement widget = driver.findElement(By.xpath(OSS_WINDOW_TAB));
         return new TabWindowWidget(driver, wait, widget);
     }
-
+    
     private TabWindowWidget(WebDriver driver, WebDriverWait wait, WebElement tabs) {
         this.driver = driver;
         this.wait = wait;
         this.tabs = tabs;
     }
-
+    
     @Override
     public void selectTabByLabel(String tabLabel) {
-        DelayUtils.waitForNestedElements(wait, this.tabs, "//div[@class='OssWindow tabWindow']");
-        WebElement allTabs = this.tabs.findElement(By.xpath("//ul[@role='tablist']"));
-        DelayUtils.waitForNestedElements(wait, allTabs, ".//button[@class='oss-tab']//div[contains(text(),'" + tabLabel + "')]");
-        WebElement tab = allTabs.findElement(By.xpath(".//button[@class='oss-tab']//div[contains(text(),'" + tabLabel + "')]"));
-        tab.click();
+        DelayUtils.waitForNestedElements(wait, this.tabs, OSS_WINDOW_TAB);
+        String xpath = ".//button[@class='oss-tab']//div[contains(text(),'" + tabLabel + "')]";
+        getTabToSelect(xpath).click();
+        
     }
-
+    
     @Override
     public void selectTabById(String ariaControls) {
-        DelayUtils.waitForNestedElements(wait, this.tabs, "//div[@class='OssWindow tabWindow']");
-        WebElement allTabs = this.tabs.findElement(By.xpath("//ul[@role='tablist']"));
-        DelayUtils.waitForNestedElements(wait, allTabs, "//button[@aria-controls='" + ariaControls + "']");
-        WebElement tab = allTabs.findElement(By.xpath("//button[@aria-controls='" + ariaControls + "']"));
-        tab.click();
+        DelayUtils.waitForNestedElements(wait, this.tabs, OSS_WINDOW_TAB);
+        String xpath = "//button[@aria-controls='" + ariaControls + "']";
+        getTabToSelect(xpath).click();
+        
     }
-
+    
     @Override
     public void callActionByLabel(String label) {
         ActionsInterface actionsContainer = ActionsContainer.createFromParent(this.tabs, driver, wait);
         actionsContainer.callActionByLabel(label);
     }
-
+    
     @Override
     public void callActionById(String id) {
         ActionsInterface actionsContainer = ActionsContainer.createFromParent(this.tabs, driver, wait);
         actionsContainer.callActionById(id);
     }
-
+    
     @Override
     public void callActionByLabel(String groupLabel, String label) {
         ActionsInterface actionsContainer = ActionsContainer.createFromParent(this.tabs, driver, wait);
         actionsContainer.callActionByLabel(groupLabel, label);
     }
-
+    
     @Override
     public void callActionById(String groupLabel, String id) {
         ActionsInterface actionsContainer = ActionsContainer.createFromParent(this.tabs, driver, wait);
         actionsContainer.callActionById(groupLabel, id);
     }
-
+    
     @Override
     public boolean isNoData(String id) {
-        List<WebElement> noData = driver.findElements(By.xpath("//div[@" + CSSUtils.TEST_ID + "='" + id + "']//h3[contains(@class,'noDataWithColumns')]"));
+        List<WebElement> noData =
+                driver.findElements(By.xpath("//div[@" + CSSUtils.TEST_ID + "='" + id + "']//h3[contains(@class,'noDataWithColumns')]"));
         return !noData.isEmpty();
     }
-
+    
     @Override
     public void clickButtonByLabel(String label) {
         ActionsInterface buttonContainer = ButtonContainer.createFromParent(this.tabs, driver, wait);
         buttonContainer.callActionByLabel(label);
     }
-
+    
     @Override
     public void callAction(String groupId, String actionId) {
         throw new RuntimeException("Method not implemented");
+    }
+    
+    private boolean isMoreVisible() {
+        DelayUtils.waitForNestedElements(wait, this.tabs, ALL_TABS);
+        WebElement allTabs = this.tabs.findElement(By.xpath(ALL_TABS));
+        List<WebElement> isMore = allTabs.findElements(By.xpath(DROPDOWN_TAB));
+        return !isMore.isEmpty();
+        
+    }
+    
+    private WebElement getTabToSelect(String xPathForTab) {
+        if (isMoreVisible()) {
+            WebElement moreTab = this.tabs.findElement(By.xpath(DROPDOWN_TAB));
+            wait.until(ExpectedConditions.elementToBeClickable(moreTab));
+            moreTab.click();
+            DelayUtils.waitByXPath(wait, xPathForTab);
+            return driver.findElement(By.xpath(xPathForTab));
+        } else {
+            return this.tabs.findElement(By.xpath(xPathForTab));
+        }
     }
 }
