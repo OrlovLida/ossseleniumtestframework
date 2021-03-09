84ccc610c5231bd3a8e64f72b3c20e9912d2d466
Jakub Skwara <jakub.skwara@comarch.com>
2021-03-09 14:19:08 +0100

    ER BIGDATA-4846 + added DFE AggregatesManagerWidget

---
+++ src/main/java/com/oss/framework/widgets/dfe/aggregatesmanager/AggregatesManagerWidget.java
@@ -0,0 +1,55 @@
+package com.oss.framework.widgets.dfe.aggregatesmanager;
+
+import com.oss.framework.utils.DelayUtils;
+import com.oss.framework.components.dfe.aggegatesmanager.AggregateSingleConfiguration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AggregatesManagerWidget {
+
+    private static final String ADD_BTN_PATH = "//button[@class='btn btn-primary btn-add-aggregate']";
+    private static final String AGGREGATES_MANAGER_PATH = "//div[@class='AggregatesManagerContainer']";
+    private static final String AGGREGATE_COMPONENT_PATH = "//section[@class='AggregateComponentContainer']";
+
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+    private final WebElement webElement;
+    private List<AggregateSingleConfiguration> aggregateConfigurations;
+
+    public static AggregatesManagerWidget create(WebDriver driver, WebDriverWait wait){
+        DelayUtils.waitByXPath(wait, AGGREGATES_MANAGER_PATH);
+        WebElement webElement = driver.findElement(By.xpath(AGGREGATES_MANAGER_PATH));
+
+        return new AggregatesManagerWidget(driver, wait, webElement);
+    }
+
+    private AggregatesManagerWidget(WebDriver driver, WebDriverWait webDriverWait, WebElement webElement) {
+        this.driver = driver;
+        this.wait = webDriverWait;
+        this.webElement = webElement;
+        this.aggregateConfigurations = this.webElement.findElements(By.xpath(AGGREGATE_COMPONENT_PATH))
+                .stream()
+                .map(element -> new AggregateSingleConfiguration(driver, wait, element))
+                .collect(Collectors.toList());
+    }
+
+    public void clickAdd() {
+        this.webElement.findElement(By.xpath(ADD_BTN_PATH)).click();
+
+        DelayUtils.waitByXPath(wait, AggregateSingleConfiguration.AGGREGATE_FORM_PATH);
+        List<WebElement> list = this.webElement.findElements(By.xpath(AGGREGATE_COMPONENT_PATH));
+        if(!list.isEmpty()){
+            AggregateSingleConfiguration addedConfig = new AggregateSingleConfiguration(driver, wait, list.get(list.size() - 1));
+            aggregateConfigurations.add(addedConfig);
+        }
+    }
+
+    public List<AggregateSingleConfiguration> getAggregateConfigurations() {
+        return this.aggregateConfigurations;
+    }
+}
