ade79dceb7fb09fd370d1b90f89bf827a4a2d2a9
Gabriela Kasza <gabriela.kasza@comarch.com>
2021-05-07 14:14:15 +0200

    ER OSSWEB-12171 # small fix for old table and editable list

--- src/main/java/com/oss/framework/widgets/tablewidget/OldTable.java
+++ src/main/java/com/oss/framework/widgets/tablewidget/OldTable.java
@@ -74,7 +74,9 @@ public class OldTable implements TableInterface {
         actions.moveToElement(table).build().perform();
         WebElement window = table.findElement(
                 By.xpath("//div[@" + CSSUtils.TEST_ID + "='" + dataAttributeName + "']/ancestor::div[contains(@class,'OssWindow')]"));
-        return new OldTable(driver, wait, dataAttributeName, table, window);
+        String dataAttributeNameWindow = window.getAttribute("data-attributename");
+        WebElement tableWindow = driver.findElement(By.xpath("//div[@" + CSSUtils.TEST_ID + "='" + dataAttributeNameWindow + "']"));
+        return new OldTable(driver, wait, dataAttributeName, table, tableWindow);
     }
 
     public static OldTable createTableForActiveTab(WebDriver driver, WebDriverWait wait){
@@ -353,7 +355,7 @@ public class OldTable implements TableInterface {
 
     private ActionsInterface getActionsInterface() {
         DelayUtils.waitForNestedElements(wait, window, CONTEXT_ACTIONS_CONTAINER);
-        boolean isNewActionContainer = isElementPresent(driver, By.className("actionsContainer"));
+        boolean isNewActionContainer = isElementPresent(window, By.className("actionsContainer"));
         if (isNewActionContainer) {
             return ActionsContainer.createFromParent(window, driver, wait);
         } else {
@@ -361,9 +363,9 @@ public class OldTable implements TableInterface {
         }
     }
 
-    private static boolean isElementPresent(WebDriver driver, By by) {
+    private static boolean isElementPresent(WebElement window, By by) {
         try {
-            driver.findElement(by);
+            window.findElement(by);
             return true;
         } catch (NoSuchElementException e) {
             return false;
