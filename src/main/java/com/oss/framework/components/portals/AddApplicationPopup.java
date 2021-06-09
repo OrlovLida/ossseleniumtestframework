e215a5611abb81963043a6aef12d0625b89c9a77
Sebastian Słupiński <sebastian.slupinski@gmail.com>
2021-06-09 14:17:20 +0200

    ER OSSWEB-9960 Create Selenium tests for new navigation # add popup for adding new application into category

---
+++ src/main/java/com/oss/framework/components/portals/AddApplicationPopup.java
@@ -0,0 +1,36 @@
+package com.oss.framework.components.portals;
+
+import com.oss.framework.components.inputs.ComponentFactory;
+import com.oss.framework.components.inputs.Input;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AddApplicationPopup extends PopupV2 {
+
+    private static final String APPLICATION_INPUT_ID = "application-input";
+    private static final String NAME_TEXT_FIELD_ID = "name";
+    private static final String DESCRIPTION_TEXT_AREA_ID = "description";
+    private static final String SAVE_BUTTON_FULL_XPATH = "//div[@class='popupBackground']//a[@class='CommonButton btn btn-primary btn-md']";
+
+    public AddApplicationPopup(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public void setApplication(String application){
+        Input applicationInput = ComponentFactory.create(APPLICATION_INPUT_ID, Input.ComponentType.COMBOBOX, driver, wait);
+        applicationInput.setSingleStringValue(application);
+    }
+
+    public void setApplicationName(String applicationName){
+        setComponentValue(NAME_TEXT_FIELD_ID, applicationName, Input.ComponentType.TEXT_FIELD);
+    }
+
+    public void setDescription(String applicationName){
+        setComponentValue(DESCRIPTION_TEXT_AREA_ID, applicationName, Input.ComponentType.TEXT_AREA);
+    }
+
+    public void clickSaveButton(){
+        driver.findElement(By.xpath(SAVE_BUTTON_FULL_XPATH)).click();
+    }
+}
