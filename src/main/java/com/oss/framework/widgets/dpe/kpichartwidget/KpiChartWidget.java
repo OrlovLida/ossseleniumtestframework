9cb6c3c43a439737cff651906d9e0f9253af345d
Agnieszka.Podolska <agnieszka.podolska@comarch.com>
2021-07-12 11:47:07 +0200

    Dodanie elementów do KPI Chart Widget niezbędnych do kolejnych testów ind view

--- src/main/java/com/oss/framework/widgets/dpe/kpichartwidget/KpiChartWidget.java
+++ src/main/java/com/oss/framework/widgets/dpe/kpichartwidget/KpiChartWidget.java
@@ -7,16 +7,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-import static com.oss.framework.logging.LoggerMessages.CLICK_BTN;
-import static com.oss.framework.logging.LoggerMessages.ELEMENT_PRESENT_AND_VISIBLE;
-import static com.oss.framework.logging.LoggerMessages.MOVE_MOUSE_OVER;
+import static com.oss.framework.logging.LoggerMessages.*;
 
 public class KpiChartWidget extends Widget {
 
@@ -42,24 +39,27 @@ public class KpiChartWidget extends Widget {
     private static final String AREA_CHART_BUTTON_PATH = "//i[@aria-label='AREA_CHART']";
     private static final String LINE_CHART_BUTTON_PATH = "//i[@aria-label='LINE_CHART']";
     private static final String BAR_CHART_BUTTON_PATH = "//i[@aria-label='BAR_CHART']";
+    private static final String CHART_COLOR_BUTTON_ID = "chart-color-button";
+    private static final String DATA_SERIES_COLOR_BUTTON_PATH = "//div[@class='colorPickerWrapper']";
+    private static final String FIRST_COLOR_BUTTON_PATH = "//div[@class='color-picker__color-table-cell']";
 
     public KpiChartWidget(WebDriver driver, WebDriverWait webDriverWait, WebElement webElement) {
         super(driver, webElement, webDriverWait);
     }
 
-    public static KpiChartWidget create(WebDriver driver, WebDriverWait wait){
+    public static KpiChartWidget create(WebDriver driver, WebDriverWait wait) {
         DelayUtils.waitByXPath(wait, KPI_CHART_WIDGET_PATH);
         WebElement webElement = driver.findElement(By.xpath(KPI_CHART_WIDGET_PATH));
 
         return new KpiChartWidget(driver, wait, webElement);
     }
 
-    public void waitForPresenceAndVisibility(){
+    public void waitForPresenceAndVisibility() {
         DelayUtils.waitForPresenceAndVisibility(webDriverWait, By.xpath(KPI_CHART_WIDGET_PATH));
         log.debug(ELEMENT_PRESENT_AND_VISIBLE + "Chart");
     }
 
-    public void hoverMouseOverPoint(){
+    public void hoverMouseOverPoint() {
         int size = getNumberOfSamples();
         log.trace("Number of samples: {}", size);
         String pointXpath = "(//*[starts-with(@class,'amcharts-Container amcharts-Bullet amcharts-CircleBullet')]//*[@class='amcharts-Sprite-group amcharts-Circle-group'])[" + (size / 2) + "]";
@@ -73,65 +73,69 @@ public class KpiChartWidget extends Widget {
         return numberOfSamples.size() - 1;
     }
 
-    private void moveOverElement(WebElement webElement){
+    private void moveOverElement(WebElement webElement) {
         Actions action = new Actions(driver);
         action.moveToElement(webElement).click().build().perform();
         log.debug(MOVE_MOUSE_OVER + "point");
     }
 
-    private void clickElementWithOffset(WebElement webElement, int offsetX, int offsetY){
+    private void clickElementWithOffset(WebElement webElement, int offsetX, int offsetY) {
         Actions action = new Actions(driver);
         action.moveToElement(webElement, offsetX, offsetY).click().build().perform();
         log.debug(MOVE_MOUSE_OVER + "point");
     }
 
-    public void maximizeChart(){
+    //TODO: it should be removed after changing DFE-SQM tests
+    public void maximizeChart() {
         showChartActions();
         clickMaximize(); // to tak nie bedzie dzialac - niepotrzebne chart actions
         hideChartActions();
         log.info("Chart maximized");
     }
 
-    public void minimizeChart(){
+    //TODO: it should be removed after changing DFE-SQM tests
+    public void minimizeChart() {
         showChartActions();
         clickMinimize();
         hideChartActions();
         log.info("Chart minimized");
     }
-// można jedną metodą - jak z logowaniem?
-    private void showChartActions(){
+
+    //TODO: it should be removed after changing DFE-SQM tests
+    private void showChartActions() {
         clickChartActionsBar(COLLAPSED_GRAPH_MENU_PATH);
         log.debug(CLICK_BTN + "Show chart actions");
     }
-// można jedną metodą - jak z logowaniem?
-    private void hideChartActions(){
+
+    //TODO: it should be removed after changing DFE-SQM tests
+    private void hideChartActions() {
         clickChartActionsBar(EXPANDED_GRAPH_MENU_PATH);
         log.debug(CLICK_BTN + "Hide chart actions");
     }
-//AP nowa metoda - zamiast show i hide chart actions
-    public void clickChartActions(){
+
+    public void clickChartActions() {
         moveOverElement(GRAPH_LOCATOR_PATH);
 
         log.debug(MOVE_MOUSE_OVER + "first chart");
         Button graphMenuButton = Button.createById(driver, CHART_ACTIONS_BUTTON_ID);
-//        DelayUtils.sleep(5000);
         graphMenuButton.click();
+        log.debug("Click chart actions button");
         DelayUtils.sleep();
     }
 
-    private void clickChartActionsBar(String actionBarXpath){
+    //TODO: it should be removed after changing DFE-SQM tests
+    private void clickChartActionsBar(String actionBarXpath) {
         moveOverElement(GRAPH_LOCATOR_PATH);
 
         log.debug(MOVE_MOUSE_OVER + "first chart");
 
-//        WebElement graphMenu = findElementByXpath(actionBarXpath);
         Button graphMenuButton = Button.createById(driver, CHART_ACTIONS_BUTTON_ID);
-//        DelayUtils.waitForClickability(webDriverWait, graphMenu);
         graphMenuButton.click();
         DelayUtils.sleep();
     }
 
-    private void clickMaximize(){
+    //TODO: it should be removed after changing DFE-SQM tests
+    private void clickMaximize() {
 //        moveOverElement(RESIZE_CHART_PATH);
         DelayUtils.waitForPresence(webDriverWait, By.xpath(MAXIMIZE_CHART_PATH)); // omija to - do zobaczenia czemu
 //        findElementByXpath(RESIZE_CHART_PATH).click();
@@ -141,28 +145,41 @@ public class KpiChartWidget extends Widget {
         log.debug(CLICK_BTN + "MAXIMIZE");
     }
 
-    private void clickMinimize(){
+    //TODO: it should be removed after changing DFE-SQM tests
+    private void clickMinimize() {
         moveOverElement(RESIZE_CHART_PATH);
         DelayUtils.waitForPresence(webDriverWait, By.xpath(MINIMIZE_CHART_PATH));
         findElementByXpath(RESIZE_CHART_PATH).click();
-
         log.debug(CLICK_BTN + "MINIMIZE");
     }
 
-    // do przeniesienia później do Page
-    public void clickAreaChartButton(){
+    //TODO: move later to KpiViewPage - when data-testid will be added
+    public void clickAreaChartButton() {
         findElementByXpath(AREA_CHART_BUTTON_PATH).click();
         log.debug(CLICK_BTN + "AREA CHART TYPE");
     }
 
-    public void clickBarChartButton(){
+    public void clickBarChartButton() {
         findElementByXpath(BAR_CHART_BUTTON_PATH).click();
         log.debug(CLICK_BTN + "BAR CHART TYPE");
     }
 
-    public void clickLineChartButton(){
+    public void clickLineChartButton() {
         findElementByXpath(LINE_CHART_BUTTON_PATH).click();
         log.debug(CLICK_BTN + "LINE CHART TYPE");
+    }
+
+    public void clickChartColorButton() {
+        Button chartColorButton = Button.createById(driver, CHART_COLOR_BUTTON_ID);
+        chartColorButton.click();
+        log.debug(CLICK_BTN + "Choose chart series color");
+    }
+
+    public void pickDataSeriesColorButton() {
+        findElementByXpath(DATA_SERIES_COLOR_BUTTON_PATH).click();
+        log.debug(CLICK_BTN + "DATA SERIES COLOR");
+        findElementByXpath(FIRST_COLOR_BUTTON_PATH).click();
+        log.debug("First color from the list is chosen");
     }
 
     private void moveOverElement(String resizeChartPath) {
@@ -170,51 +187,52 @@ public class KpiChartWidget extends Widget {
         action.moveToElement(findElementByXpath(resizeChartPath)).build().perform();
     }
 
-    public int countColumns(){
+    public int countColumns() {
         int columnsCount = this.webElement.findElements(By.xpath(CHART_COLUMN_PATH)).size();
         log.debug("Columns count: {}", columnsCount);
         return columnsCount;
     }
 
-    public int countLines(){
+    public int countLines() {
         int linesCount = this.webElement.findElements(By.xpath(CHART_LINE_PATH)).size();
         log.debug("Lines count: {}", linesCount);
         return linesCount;
     }
 
-    public String dataSeriesLineWidth(){
-        WebElement chart = driver.findElement(By.xpath(CHART_LINE_PATH));
-        String strokeWidth = chart.getCssValue("stroke-width");
+    public String dataSeriesLineWidth() {
+        String strokeWidth = findElementByXpath(CHART_LINE_PATH).getCssValue("stroke-width");
         log.debug("Line width: {}", strokeWidth);
         return strokeWidth;
     }
 
-    public String dataSeriesVisibility(){
-        WebElement chart = driver.findElement(By.xpath(CHART_LINE_PATH));
-        String dataSeriesVisibility = chart.getCssValue("visibility");
+    public String dataSeriesVisibility() {
+        String dataSeriesVisibility = findElementByXpath(CHART_LINE_PATH).getCssValue("visibility");
         log.debug("Data Series visibility: {}", dataSeriesVisibility);
         return dataSeriesVisibility;
     }
 
-    public String dataSeriesFillOpacity(){
-        WebElement chart = driver.findElement(By.xpath(CHART_LINE_PATH));
-        String fillOpacity = chart.getCssValue("fill-opacity");
+    public String dataSeriesFillOpacity() {
+        String fillOpacity = findElementByXpath(CHART_LINE_PATH).getCssValue("fill-opacity");
         log.debug("Data Series fill opacity: {}", fillOpacity);
         return fillOpacity;
     }
 
-    public String barDataSeriesFillOpacity(){
-        WebElement chart = driver.findElement(By.xpath(BARCHART_PATH));
-        String fillOpacity = chart.getCssValue("fill-opacity");
+    public String barDataSeriesFillOpacity() {
+        String fillOpacity = findElementByXpath(BARCHART_PATH).getCssValue("fill-opacity");
         log.debug("Bar chart Data Series fill opacity: {}", fillOpacity);
         return fillOpacity;
     }
 
-    public void clickDataSeriesLegend(){
-        DelayUtils.waitForPageToLoad(driver, webDriverWait);
-        WebElement legend = driver.findElement(By.xpath(LEGEND_PATH));
-        clickElementWithOffset(legend, 0, -5);
+    public String dataSeriesColor() {
+        String strokeColor = findElementByXpath(CHART_LINE_PATH).getCssValue("stroke");
+        log.debug("Data Series color: {}", strokeColor);
+        return strokeColor;
+    }
 
+    public void clickDataSeriesLegend() {
+        DelayUtils.waitForPageToLoad(driver, webDriverWait);
+        WebElement legend = findElementByXpath(LEGEND_PATH);
+        clickElementWithOffset(legend, 0, -5);
         log.debug("Clicking first data series on legend");
     }
 
