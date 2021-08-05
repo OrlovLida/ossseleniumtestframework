6cfc33f7460e0f2b9de372b7b9bbba52e657189e
Gabriela Kasza <gabriela.kasza@comarch.com>
2021-08-05 17:48:26 +0200

    ER OSSWEB-13577 # correction after CR

--- src/main/java/com/oss/framework/components/inputs/MultiCombobox.java
+++ src/main/java/com/oss/framework/components/inputs/MultiCombobox.java
@@ -13,6 +13,12 @@ import com.oss.framework.utils.DelayUtils;
 
 public class MultiCombobox extends Input {
     
+    private final static String TITLE_ITEM_CONTAINS_XPATH = ".//div[contains(@title,'%s')]";
+    private final static String TITLE_ITEM_EQUAL_XPATH = ".//div[@title='%s']";
+    private final static String ANGLE_UP_XPATH = ".//i[contains(@class,'angle-up')]";
+    private final static String CLEAR_XPATH =
+            ".//div[@" + CSSUtils.TEST_ID + "='%s-input']//i[contains(@class,'OSSIcon ossfont-close combo-box__close')]";
+    
     static MultiCombobox create(WebDriver driver, WebDriverWait wait, String componentId) {
         return new MultiCombobox(driver, wait, componentId);
     }
@@ -33,31 +39,22 @@ public class MultiCombobox extends Input {
     public void setValue(Data value) {
         Actions actions = new Actions(driver);
         actions.moveToElement(webElement).click().build().perform();
+        waitForSpinners();
         if (isSearchEnabled()) {
-            WebElement input = webElement.findElement(By.xpath(createDropdownSearchInputPath()));
-            input.sendKeys(value.getStringValue());
-            DelayUtils.sleep(); // TODO: wait for spinners
-            acceptStringValue(input);
-        }
-        WebElement dropdown = webElement.findElement(By.xpath("//div[@" + CSSUtils.TEST_ID + "='" + componentId + "-dropdown']"));
-        WebElement item = dropdown.findElement(By.xpath(".//div[@title='" + value.getStringValue() + "']"));
-        actions.moveToElement(item).click(item).build().perform();
+            searchItem(value.getStringValue());
+        } else
+            chooseItem(String.format(TITLE_ITEM_EQUAL_XPATH, value.getStringValue()));
     }
     
     @Override
     public void setValueContains(Data value) {
         Actions actions = new Actions(driver);
         actions.moveToElement(webElement).click().build().perform();
+        waitForSpinners();
         if (isSearchEnabled()) {
-            WebElement input = webElement.findElement(By.xpath(createDropdownSearchInputPath()));
-            input.sendKeys(value.getStringValue());
-            DelayUtils.sleep(); // TODO: wait for spinners
-            acceptStringValue(input);
-        }
-        WebElement dropdown = webElement.findElement(By.xpath("//div[@" + CSSUtils.TEST_ID + "='" + componentId + "-dropdown']"));
-        WebElement item = dropdown.findElement(By.xpath(".//div[contains(@title,'" + value.getStringValue() + "')]"));
-        actions.moveToElement(item).click(item).build().perform();
-
+            searchItem(value.getStringValue());
+        } else
+            chooseItem(String.format(TITLE_ITEM_CONTAINS_XPATH, value.getStringValue()));
     }
     
     private void acceptStringValue(WebElement input) {
@@ -73,8 +70,7 @@ public class MultiCombobox extends Input {
     
     @Override
     public void clear() {
-        webElement.findElement(By.xpath(".//div[@" + CSSUtils.TEST_ID + "='" + componentId
-                + "-input']//i[contains(@class,'OSSIcon ossfont-close combo-box__close')]")).click();
+        webElement.findElement(By.xpath(String.format(CLEAR_XPATH, componentId))).click();
     }
     
     private String createDropdownSearchInputPath() {
@@ -89,4 +85,29 @@ public class MultiCombobox extends Input {
     private boolean isSearchEnabled() {
         return !webElement.findElements(By.xpath(createDropdownSearchInputPath())).isEmpty();
     }
+    
+    private void searchItem(String value) {
+        WebElement input = webElement.findElement(By.xpath(createDropdownSearchInputPath()));
+        input.sendKeys(value);
+        DelayUtils.sleep(); // TODO: wait for spinners
+        acceptStringValue(input);
+    }
+    
+    private void chooseItem(String xpath) {
+        Actions actions = new Actions(driver);
+        WebElement dropdown = webElement.findElement(By.xpath(createDropdownList()));
+        WebElement item = dropdown.findElement(By.xpath(xpath));
+        actions.moveToElement(item).click(item).build().perform();
+    }
+    
+    private String createDropdownList() {
+        return "//div[@" + CSSUtils.TEST_ID + "='" + componentId + "-dropdown']";
+    }
+    
+    private void waitForSpinners() {
+        WebElement openInput = webElement.findElement(By.xpath(ANGLE_UP_XPATH));
+        DelayUtils.waitForVisibility(webDriverWait, openInput);
+        DelayUtils.sleep();
+    }
+    
 }
