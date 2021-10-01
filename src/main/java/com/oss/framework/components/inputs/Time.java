22e771c5aed9be4b25c2c840ee239bdfa46049b7
Gabriela Kasza <gabriela.kasza@comarch.com>
2021-10-01 11:31:42 +0200

    ER OSSWEB-12992 # correction after CR

--- src/main/java/com/oss/framework/components/inputs/Time.java
+++ src/main/java/com/oss/framework/components/inputs/Time.java
@@ -1,6 +1,7 @@
 package com.oss.framework.components.inputs;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -38,7 +39,9 @@ public class Time extends Input {
     
     @Override
     public void clear() {
-        webElement.findElement(By.xpath(".//input")).clear();
+        WebElement input = webElement.findElement(By.xpath(".//input"));
+        input.sendKeys(Keys.CONTROL + "a");
+        input.sendKeys(Keys.DELETE);
     }
     
     @Override
