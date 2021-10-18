888aa34a9a9e820ce00e24014fcbf22fbe7738fc
Agnieszka.Podolska <agnieszka.podolska@comarch.com>
2021-10-18 20:14:36 +0200

    poprawki po CR

--- src/main/java/com/oss/framework/listwidget/iaa/ListApp.java
+++ src/main/java/com/oss/framework/listwidget/iaa/ListApp.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ListApp {
 
     private static final Logger log = LoggerFactory.getLogger(ListApp.class);
@@ -35,12 +38,16 @@ public class ListApp {
         this.listApp = listApp;
     }
 
-    public String getTextFromAppList(){
-        DelayUtils.waitForPageToLoad(driver, wait);
-        WebElement text = listApp.findElement(By.xpath(".//div[contains(@class, 'first last')]//div[contains(@class, 'text-default')]"));
-        String displayedText = text.getText();
-        log.debug("Text {} is displayed on the appList", displayedText);
-        return displayedText;
+    public List<WebElement> getRows(){
+        return listApp.findElements(By.xpath(".//div[contains(@class, 'first last')]"));
     }
 
+    public List<String> getValue(){
+        DelayUtils.waitForPageToLoad(driver, wait);
+        List<String> values = getRows()
+                .stream().map(row->row.findElement(By.xpath(".//div[contains(@class, 'text-default')]")))
+                .map(text->text.getText()).collect(Collectors.toList());
+        log.debug("Getting all values from app list");
+        return values;
+    }
 }
