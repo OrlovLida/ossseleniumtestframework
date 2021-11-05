413972c01f4e2cac1cdda009acdac2d99759bd53
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2021-11-05 14:26:45 +0100

    ER OSSTEST-111422 # Notifications corrections

--- src/main/java/com/oss/framework/mainheader/Notifications.java
+++ src/main/java/com/oss/framework/mainheader/Notifications.java
@@ -1,6 +1,5 @@
 package com.oss.framework.mainheader;
 
-import com.oss.framework.utils.DelayUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -9,13 +8,15 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.oss.framework.utils.DelayUtils;
+
 public class Notifications implements NotificationsInterface {
 
-    private static final Logger log = LoggerFactory.getLogger(DelayUtils.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Notifications.class);
+    private static final By NOTIFICATION = By.xpath("//div[@class='notifications-button']");
     private static final By CLEAR_NOTIFICATION = By.xpath("//a[@class='clear-action']");
-    private static final By NOTIFICATION_BUTTON = By.xpath("//div[@class='notificationHoveringContainer']");
-    private static final By NOTIFICATION_OPENED = By.xpath("//a[(@class='clicked badge') or (@class = 'clicked notificationType badge')]");
-    private static final By NOTIFICATION_CLOSED = By.xpath("//a[(@class= 'badge') or (@class= 'notificationType badge')]");
+    private static final By NOTIFICATION_BUTTON = By.xpath("//div[@class='notifications-button']/button");
+    private static final By NOTIFICATION_OPENED = By.xpath("//div[@class='notifications__panel__wrapper']");
     private static final By EMPTY_NOTIFICATION = By.xpath("//div[@class='notificationEmpty']");
     private static final By NOTIFICATION_LIST = By.xpath("//div[@class='notificationContainer']/div[not(@class = 'notificationEmpty')]");
     private static final By NOTIFICATION_DETAILS = By.xpath("(//a[@class='detailsLink'])[1]");
@@ -24,23 +25,14 @@ public class Notifications implements NotificationsInterface {
     private final WebDriver driver;
     private final WebDriverWait wait;
 
-    private Notifications(WebDriver driver, WebDriverWait wait) {
-        this.driver = driver;
-        this.wait = wait;
-    }
-
     public static NotificationsInterface create(WebDriver driver, WebDriverWait wait) {
-        DelayUtils.waitByXPath(wait, "//div[@class='toolbarWidget globalNotification']");
+        DelayUtils.waitBy(wait, NOTIFICATION);
         return new Notifications(driver, wait);
     }
 
-    private static boolean isElementPresent(WebDriver driver, By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
+    private Notifications(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
     }
 
     @Override
@@ -55,7 +47,7 @@ public class Notifications implements NotificationsInterface {
                         .getText();
         driver.findElement(CLEAR_NOTIFICATION).click();
         closeNotificationContainer();
-        log.info("Notification finished with message: " + notificationText);
+        LOGGER.info("Notification finished with message: {}", notificationText);
         return notificationText;
     }
 
@@ -77,22 +69,8 @@ public class Notifications implements NotificationsInterface {
     @Override
     public int getAmountOfNotifications() {
         int amount = driver.findElements(NOTIFICATION_LIST).size();
-        log.info("The list of notifications includes " + amount + " elements");
+        LOGGER.info("The list of notifications includes {} elements", amount);
         return amount;
-    }
-
-    private void openNotificationContainer() {
-        if (isElementPresent(driver, NOTIFICATION_CLOSED)) {
-            driver.findElement(NOTIFICATION_BUTTON).click();
-            DelayUtils.waitBy(wait, NOTIFICATION_OPENED);
-        }
-    }
-
-    private void closeNotificationContainer() {
-        if (isElementPresent(driver, NOTIFICATION_OPENED)) {
-            driver.findElement(NOTIFICATION_BUTTON).click();
-            DelayUtils.waitBy(wait, NOTIFICATION_CLOSED);
-        }
     }
 
     public void openDetailsForSpecificNotification(String text, String notificationStatus) {
@@ -104,5 +82,27 @@ public class Notifications implements NotificationsInterface {
 
     public void clickDownloadFile() {
         driver.findElement(DOWNLOAD_FILE).click();
+    }
+
+    private void openNotificationContainer() {
+        if (!isElementPresent(driver, NOTIFICATION_OPENED)) {
+            driver.findElement(NOTIFICATION_BUTTON).click();
+            DelayUtils.waitBy(wait, NOTIFICATION_OPENED);
+        }
+    }
+
+    private void closeNotificationContainer() {
+        if (isElementPresent(driver, NOTIFICATION_OPENED)) {
+            driver.findElement(NOTIFICATION_BUTTON).click();
+        }
+    }
+
+    private static boolean isElementPresent(WebDriver driver, By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 }
