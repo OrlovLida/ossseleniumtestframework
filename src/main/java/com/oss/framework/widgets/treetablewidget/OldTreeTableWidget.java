a81ffe88533e55b1416f82b666854f6e09e146d8
Sebastian Słupiński <sebastian.slupinski@gmail.com>
2021-11-16 18:15:08 +0100

    ER OSSWEB-13103 [Selenium] Add suport for selection bar in table widget # remove unnecessary changes in OldTreeTableWidget

--- src/main/java/com/oss/framework/widgets/treetablewidget/OldTreeTableWidget.java
+++ src/main/java/com/oss/framework/widgets/treetablewidget/OldTreeTableWidget.java
@@ -6,36 +6,40 @@
  */
 package com.oss.framework.widgets.treetablewidget;
 
-import com.oss.framework.utils.DelayUtils;
-import com.oss.framework.widgets.Widget;
-import com.oss.framework.widgets.tablewidget.OldTable;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.oss.framework.utils.CSSUtils;
+import com.oss.framework.utils.DelayUtils;
+import com.oss.framework.widgets.Widget;
+import com.oss.framework.widgets.tablewidget.OldTable;
 
 /**
  * @author Comarch
  */
 public class OldTreeTableWidget extends Widget {
+    public static OldTreeTableWidget create(WebDriver driver, WebDriverWait wait, String dataAttributeName) {
+        return new OldTreeTableWidget(driver, wait, dataAttributeName);
 
+    }
     private final String id;
 
     private OldTreeTableWidget(WebDriver driver, WebDriverWait wait, String id) {
-        super(driver, wait, id);
-        this.id = id;
-    }
+      super(driver,wait,id);
+       this.id =id;
 
-    public static OldTreeTableWidget create(WebDriver driver, WebDriverWait wait, String dataAttributeName) {
-        return new OldTreeTableWidget(driver, wait, dataAttributeName);
+
     }
 
     private OldTable createTable() {
-        return OldTable.createByComponentDataAttributeName(driver, webDriverWait, id);
+        return OldTable.createByComponentDataAttributeName(driver, webDriverWait,id );
     }
 
     public List<String> getAllVisibleNodes(String attributeNameLabel) {
@@ -53,17 +57,15 @@ public class OldTreeTableWidget extends Widget {
         new Node(driver, webDriverWait, rowNumber).expandNode();
     }
 
-    public void selectNode(String value, String attributeNameLabel) {
+    public void selectNode(String value, String attributeNameLabel){
         int rowNumber = createTable().getRowNumber(value, attributeNameLabel);
         createTable().selectRow(rowNumber);
     }
-
     public void collapseNode(String value, String attributeNameLabel) {
         int rowNumber = createTable().getRowNumber(value, attributeNameLabel);
         new Node(driver, webDriverWait, rowNumber).collapseNode();
     }
-
-    public void callActionById(String id) {
+    public void callActionById(String id){
         createTable().callAction(id);
     }
 
@@ -89,10 +91,11 @@ public class OldTreeTableWidget extends Widget {
         private void expandNode() {
 
             WebElement node = getNodeExpandIcons().get(row);
-            if (!isExpanded(node)) {
+            if (!isExpanded(node)){
                 wait.until(ExpectedConditions.elementToBeClickable(node.findElement(By.xpath(TREE_NODE_ADD_ICON_XPATH))));
                 node.findElement(By.xpath(TREE_NODE_ADD_ICON_XPATH)).click();
-                DelayUtils.waitForPageToLoad(driver, wait);
+                DelayUtils.waitForPageToLoad(driver,wait);
+
             }
         }
 
@@ -101,7 +104,7 @@ public class OldTreeTableWidget extends Widget {
             if (isExpanded(node)) {
                 wait.until(ExpectedConditions.elementToBeClickable(node.findElement(By.xpath(TREE_NODE_MINUS_ICON_XPATH))));
                 node.findElement(By.xpath(TREE_NODE_MINUS_ICON_XPATH)).click();
-                DelayUtils.waitForPageToLoad(driver, wait);
+                DelayUtils.waitForPageToLoad(driver,wait);
             }
         }
 
