ce93b27b6af930dd7d584b7f45bebd3cc6bf53d3
Gabriela Kasza <gabriela.kasza@comarch.com>
2022-01-10 15:17:46 +0100

    ER OSSWEB-16215 # Fix layout change selenium tests for Inventory View

--- src/main/java/com/oss/framework/mainheader/ButtonPanel.java
+++ src/main/java/com/oss/framework/mainheader/ButtonPanel.java
@@ -6,68 +6,72 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.oss.framework.components.portals.ChooseConfigurationWizard;
+import com.oss.framework.components.portals.DropdownList;
 import com.oss.framework.components.portals.SaveConfigurationWizard;
+import com.oss.framework.utils.CSSUtils;
 import com.oss.framework.utils.DelayUtils;
 
 public class ButtonPanel {
 
     private static final String BUTTON_PANEL_XPATH = "//div[@class='view-actions-container']";
-    private static final String ICON_XPATH_PATTERN = ".//i[contains(@class,'%s')]";
-    private static final String LAYOUT_ICON_ID = "layout";
-    private static final String LAYOUT_EXPANDER_ICON_ID = "fa fa-chevron-down";
+    private static final String ICON_XPATH_PATTERN = ".//*[contains(@"+ CSSUtils.TEST_ID +",'%s')]";
     private static final String SAVE_CONFIGURATION_ICON_ID = "fa fa-fw fa-floppy-o";
     private static final String CHOOSE_CONFIGURATION_ICON_ID = "fa fa-fw fa-cog";
     private static final String DOWNLOAD_CONFIGURATION_ICON_ID = "fa fa-fw fa-download";
+    private static final String BUTTON_PANEL_CSS = "div.view-actions-container";
+    private static final String HORIZONTAL_LAYOUT_BUTTON_CLASS = "ossfont-layout-horizontal";
 
     private final WebDriver driver;
     private final WebDriverWait wait;
+    private final WebElement webElement;
 
-    private ButtonPanel(WebDriver driver, WebDriverWait wait) {
+    private ButtonPanel(WebDriver driver, WebDriverWait wait, WebElement webElement) {
         this.driver = driver;
         this.wait = wait;
+        this.webElement = webElement;
     }
 
     public static ButtonPanel create(WebDriver driver, WebDriverWait wait) {
-        return new ButtonPanel(driver, wait);
+        WebElement buttonPanel = driver.findElement(By.cssSelector(BUTTON_PANEL_CSS));
+
+        return new ButtonPanel(driver, wait, buttonPanel);
     }
 
-    public void clickOnIcon(String iconId) {
-        getButtonIcon(iconId).click();
+    public void clickButton(String buttonId) {
+        getButton(buttonId).click();
     }
 
-    public void expandLayoutMenu() {
-        if (isButtonDisplayed(LAYOUT_EXPANDER_ICON_ID))
-            clickOnIcon(LAYOUT_ICON_ID);
+    public void clickButton(String groupId, String actionId){
+        clickButton(groupId);
+        DropdownList.create(driver, wait).selectOptionWithId(actionId);
     }
 
     public SaveConfigurationWizard openSaveConfigurationWizard() {
-        clickOnIcon(SAVE_CONFIGURATION_ICON_ID);
+        clickButton(SAVE_CONFIGURATION_ICON_ID);
         return SaveConfigurationWizard.create(driver, wait);
     }
 
     public ChooseConfigurationWizard openChooseConfigurationWizard() {
-        clickOnIcon(CHOOSE_CONFIGURATION_ICON_ID);
+        clickButton(CHOOSE_CONFIGURATION_ICON_ID);
         return ChooseConfigurationWizard.create(driver, wait);
     }
 
     public ChooseConfigurationWizard openDownloadConfigurationWizard() {
-        clickOnIcon(DOWNLOAD_CONFIGURATION_ICON_ID);
+        clickButton(DOWNLOAD_CONFIGURATION_ICON_ID);
         return ChooseConfigurationWizard.create(driver, wait);
+    }
+    public boolean isHorizontalLayout(){
+        return !webElement.findElements(By.className(HORIZONTAL_LAYOUT_BUTTON_CLASS)).isEmpty();
     }
 
     private WebElement getButtonPanel() {
         DelayUtils.waitByXPath(wait, BUTTON_PANEL_XPATH);
-        return driver.findElement(By.xpath(BUTTON_PANEL_XPATH));
+        return webElement.findElement(By.xpath(BUTTON_PANEL_XPATH));
     }
 
-    private WebElement getButtonIcon(String iconId) {
+    private WebElement getButton(String iconId) {
         DelayUtils.waitByXPath(wait, String.format(ICON_XPATH_PATTERN, iconId));
         return getButtonPanel().findElement(By.xpath(String.format(ICON_XPATH_PATTERN, iconId)));
-    }
-
-    private boolean isButtonDisplayed(String iconId) {
-        DelayUtils.waitByXPath(wait, String.format(ICON_XPATH_PATTERN, iconId));
-        return !getButtonPanel().findElements(By.xpath(String.format(ICON_XPATH_PATTERN, iconId))).isEmpty();
     }
 
 }
