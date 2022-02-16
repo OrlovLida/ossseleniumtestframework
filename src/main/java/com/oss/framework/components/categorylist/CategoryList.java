03b38723d21ba75905d82dc1faa3620a76386407
Gabriela Kasza <gabriela.kasza@comarch.com>
2022-02-16 13:17:21 +0100

    ER OSSWEB-16228 # create  tests for new navigation

--- src/main/java/com/oss/framework/components/categorylist/CategoryList.java
+++ src/main/java/com/oss/framework/components/categorylist/CategoryList.java
@@ -12,12 +12,12 @@ import java.util.stream.Collectors;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.oss.framework.components.contextactions.InlineMenu;
 import com.oss.framework.utils.CSSUtils;
 import com.oss.framework.utils.DelayUtils;
+import com.oss.framework.utils.WebElementUtils;
 
 /**
  * @author Gabriela Zaranek
@@ -26,51 +26,51 @@ public class CategoryList {
     private static final String COLLAPSE_ICON_XPATH = ".//i[contains(@class,'chevron-up')]";
     private static final String CATEGORY_NAME_XPATH = "categoryLabel-text";
     private static final String EXPAND_ICON_XPATH = ".//i[contains(@class,'chevron-down')]";
-
+    private static final String CATEGORY_LIST_ELEMENT_CSS = ".categoryListElement";
+    
     private WebDriver driver;
     private WebDriverWait wait;
     private WebElement categoryElement;
-
-    private CategoryList(WebDriver driver, WebDriverWait wait, WebElement categoryElement){
+    
+    private CategoryList(WebDriver driver, WebDriverWait wait, WebElement categoryElement) {
         this.driver = driver;
         this.wait = wait;
         this.categoryElement = categoryElement;
     }
-
-    public static List<CategoryList> create (WebDriver driver, WebDriverWait wait, String widgetId){
+    
+    public static List<CategoryList> create(WebDriver driver, WebDriverWait wait, String widgetId) {
         DelayUtils.waitBy(wait, By.cssSelector("[" + CSSUtils.TEST_ID + "='" + widgetId + "']"));
         WebElement widget = driver.findElement(By.cssSelector("[" + CSSUtils.TEST_ID + "='" + widgetId + "']"));
-        return widget.findElements(By.cssSelector(".categoryListElement")).stream().map(category -> new CategoryList(driver, wait,category)).collect(Collectors.toList());
+        return widget.findElements(By.cssSelector(CATEGORY_LIST_ELEMENT_CSS)).stream()
+                .map(category -> new CategoryList(driver, wait, category)).collect(Collectors.toList());
     }
-
+    
     public String getValue() {
         return categoryElement.findElement(By.className(CATEGORY_NAME_XPATH)).getText();
     }
-
+    
     public void callAction(String groupId, String actionId) {
         InlineMenu.create(categoryElement, driver, wait).callAction(groupId, actionId);
     }
-
+    
     public void callAction(String actionId) {
         InlineMenu.create(categoryElement, driver, wait).callAction(actionId);
     }
-
+    
     public void expandCategory() {
         if (!isExpanded()) {
-            Actions actions = new Actions(driver);
-            actions.moveToElement(categoryElement.findElement(By.xpath(EXPAND_ICON_XPATH))).click().build().perform();
+            WebElementUtils.clickWebElement(driver, categoryElement.findElement(By.xpath(EXPAND_ICON_XPATH)));
         }
         DelayUtils.waitForElementToLoad(wait, categoryElement);
     }
-
+    
     public void collapseCategory() {
         if (isExpanded()) {
-            Actions actions = new Actions(driver);
-            actions.moveToElement(categoryElement.findElement(By.xpath(COLLAPSE_ICON_XPATH))).click().build().perform();
+            WebElementUtils.clickWebElement(driver, categoryElement.findElement(By.xpath(COLLAPSE_ICON_XPATH)));
         }
         DelayUtils.waitForElementToLoad(wait, categoryElement);
     }
-
+    
     private boolean isExpanded() {
         return !categoryElement.findElements(By.xpath(COLLAPSE_ICON_XPATH)).isEmpty();
     }
