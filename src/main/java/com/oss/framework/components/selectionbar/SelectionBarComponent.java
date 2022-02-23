eec7b9d1b3d02e218343e186eec262569f4737cf
Faustyna.Szczepanik <faustyna.szczepanik@comarch.com>
2022-02-23 09:38:48 +0100

    ER OSSWEB-16932 #XPath corrected

--- src/main/java/com/oss/framework/components/selectionbar/SelectionBarComponent.java
+++ src/main/java/com/oss/framework/components/selectionbar/SelectionBarComponent.java
@@ -10,59 +10,57 @@ import com.oss.framework.utils.DelayUtils;
 
 public class SelectionBarComponent {
 
-    private static final String SELECTION_BAR_SELECTED_OBJECTS_COUNT_LABEL_CSS = "[" + CSSUtils.TEST_ID + "=selected-objects-count-label";
-    private static final String SHOW_ONLY_SELECTED_BUTTON_CSS = "[" + CSSUtils.TEST_ID + "=show-selected-only-button";
-    private static final String UNSELECT_ALL_BUTTON_CSS = "[" + CSSUtils.TEST_ID + "=unselect-all-button";
+    private static final String SELECTION_BAR_SELECTED_OBJECTS_COUNT_LABEL_CSS = "[" + CSSUtils.TEST_ID + "=selected-objects-count-label]";
+    private static final String SHOW_ONLY_SELECTED_BUTTON_CSS = "[" + CSSUtils.TEST_ID + "=show-selected-only-button]";
+    private static final String UNSELECT_ALL_BUTTON_CSS = "[" + CSSUtils.TEST_ID + "=unselect-all-button]";
     private static final String VISIBLE_SELECTION_BAR_CSS = ".selection-bar";
     private static final String TOGGLE_BUTTON_CSS = "[" + CSSUtils.TEST_ID + "=selection-bar-toggler-button]";
-    private static final String SHOW_SELECTED_ONLY_BUTTON_ACTIVE_CSS = "[" + CSSUtils.TEST_ID + "='show-selected-only-button]";
-    
 
     private final WebDriverWait wait;
     private final WebElement widget;
-    
-    private SelectionBarComponent( WebDriverWait wait, WebElement widget) {
+
+    private SelectionBarComponent(WebDriverWait wait, WebElement widget) {
         this.wait = wait;
         this.widget = widget;
     }
-    
+
     public static SelectionBarComponent create(WebDriver driver, WebDriverWait wait, String widgetId) {
         WebElement widget = driver.findElement(By.cssSelector("[" + CSSUtils.TEST_ID + "='" + widgetId + "']"));
         return new SelectionBarComponent(wait, widget);
     }
-    
+
     public void openSelectionBar() {
         if (!isActive()) {
             widget.findElement(By.cssSelector(TOGGLE_BUTTON_CSS)).click();
-            DelayUtils.waitForPresence(wait,By.cssSelector(VISIBLE_SELECTION_BAR_CSS));
+            DelayUtils.waitForPresence(wait, By.cssSelector(VISIBLE_SELECTION_BAR_CSS));
         }
     }
-    
+
     public void hideSelectionBar() {
         if (isActive()) {
             widget.findElement(By.cssSelector(TOGGLE_BUTTON_CSS)).click();
         }
     }
-    
+
     public void unselectAll() {
         if (!isActive()) {
             openSelectionBar();
         }
         widget.findElement(By.cssSelector(UNSELECT_ALL_BUTTON_CSS)).click();
     }
-    
+
     public void showSelected() {
         if (isActive() && isShowSelectedPresent()) {
             widget.findElement(By.cssSelector(SHOW_ONLY_SELECTED_BUTTON_CSS)).click();
         }
     }
-    
+
     public void showAll() {
         if (isActive() && !isShowSelectedPresent()) {
             widget.findElement(By.cssSelector(SHOW_ONLY_SELECTED_BUTTON_CSS)).click();
         }
     }
-    
+
     public String getSelectedObjectsCount() {
         if (!isActive()) {
             openSelectionBar();
@@ -70,12 +68,12 @@ public class SelectionBarComponent {
         return widget.findElement(By.cssSelector(SELECTION_BAR_SELECTED_OBJECTS_COUNT_LABEL_CSS))
                 .getText();
     }
-    
+
     private boolean isActive() {
         return !widget.findElements(By.cssSelector(VISIBLE_SELECTION_BAR_CSS)).isEmpty();
     }
-    
+
     private boolean isShowSelectedPresent() {
-        return !widget.findElements(By.xpath(SHOW_SELECTED_ONLY_BUTTON_ACTIVE_CSS)).isEmpty();
+        return !widget.findElements(By.cssSelector(SHOW_ONLY_SELECTED_BUTTON_CSS)).isEmpty();
     }
 }
