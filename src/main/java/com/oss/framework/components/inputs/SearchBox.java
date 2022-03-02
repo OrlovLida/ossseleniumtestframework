cb9a07488198dc33be5bb1b618fa2c5533f5dfe9
Gabriela Kasza <gabriela.kasza@comarch.com>
2022-03-02 16:30:22 +0100

    ER OSSWEB-11839 # refactor SearchField input

---
+++ src/main/java/com/oss/framework/components/inputs/SearchBox.java
@@ -0,0 +1,94 @@
+/* @(#) $$Id$$
+ *
+ * Copyright (c) 2000-2022 Comarch SA All Rights Reserved. Any usage,
+ * duplication or redistribution of this software is allowed only according to
+ * separate agreement prepared in written between Comarch and authorized party.
+ */
+package com.oss.framework.components.inputs;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.oss.framework.components.data.Data;
+import com.oss.framework.components.portals.DropdownList;
+import com.oss.framework.utils.DelayUtils;
+
+/**
+ * @author Gabriela Zaranek
+ */
+public class SearchBox extends Input {
+    
+    private static final String INPUT_CSS = "input";
+    private static final String DROPDOWN_CSS = ".ExtendedSearchResults.open";
+    private static final String DATA_VALUE = "data-value";
+    
+    SearchBox(WebDriver driver, WebDriverWait webDriverWait, String componentId) {
+        super(driver, webDriverWait, componentId);
+    }
+    
+    SearchBox(WebElement parent, WebDriver driver, WebDriverWait webDriverWait, String componentId) {
+        super(parent, driver, webDriverWait, componentId);
+    }
+    
+    public static SearchBox create(WebDriver driver, WebDriverWait wait, String componentId) {
+        return new SearchBox(driver, wait, componentId);
+    }
+    
+    public static SearchBox createFromParent(WebElement parent, WebDriver driver, WebDriverWait wait, String componentId) {
+        return new SearchBox(parent, driver, wait, componentId);
+    }
+    
+    @Override
+    public void setValueContains(Data value) {
+        typeValue(value.getStringValue());
+        if (isDropdownPresent()) {
+            getDropdown().selectOptionContains(value.getStringValue());
+        }
+    }
+    
+    @Override
+    public Data getValue() {
+        return Data.createSingleData(webElement.findElement(By.cssSelector(INPUT_CSS)).getAttribute(DATA_VALUE));
+    }
+    
+    @Override
+    public void setValue(Data value) {
+        typeValue(value.getStringValue());
+        if (isDropdownPresent()) {
+            getDropdown().selectOption(value.getStringValue());
+        }
+    }
+    
+    @Override
+    public void clear() {
+        DelayUtils.waitForClickability(webDriverWait, webElement);
+        WebElement input = webElement.findElement(By.cssSelector(INPUT_CSS));
+        input.sendKeys(Keys.CONTROL + "a");
+        input.sendKeys(Keys.DELETE);
+    }
+    
+    private void typeValue(String value) {
+        clear();
+        webElement.click();
+        webElement.findElement(By.cssSelector(INPUT_CSS)).sendKeys(value);
+        Actions action = new Actions(driver);
+        action.moveToElement(webElement)
+                .click()
+                .sendKeys(Keys.ENTER)
+                .perform();
+        
+    }
+    
+    private boolean isDropdownPresent() {
+        DelayUtils.sleep(3000);
+        return !driver.findElements(By.cssSelector(DROPDOWN_CSS)).isEmpty();
+    }
+    
+    private DropdownList getDropdown() {
+        return DropdownList.create(driver, webDriverWait);
+    }
+}
--- src/main/java/com/oss/framework/components/inputs/SearchField.java
+++ src/main/java/com/oss/framework/components/inputs/SearchField.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.oss.framework.components.data.Data;
@@ -12,79 +11,60 @@ import com.oss.framework.components.portals.DropdownList;
 import com.oss.framework.utils.DelayUtils;
 
 public class SearchField extends Input {
-
-    private static final String INPUT = ".//input";
-
+    
+    private static final String INPUT_XPATH = ".//input";
+    
     private SearchField(WebDriver driver, WebDriverWait wait, String componentId) {
         super(driver, wait, componentId);
     }
-
+    
     private SearchField(WebElement parent, WebDriver driver, WebDriverWait wait, String componentId) {
         super(parent, driver, wait, componentId);
     }
-
+    
     static SearchField create(WebDriver driver, WebDriverWait wait, String componentId) {
         return new SearchField(driver, wait, componentId);
     }
-
+    
     static SearchField createFromParent(WebElement parent, WebDriver driver, WebDriverWait wait, String componentId) {
         return new SearchField(parent, driver, wait, componentId);
     }
-
-    public void typeValue(String value) {
-        DelayUtils.waitForClickability(webDriverWait, webElement);
-        webElement.click();
-        webElement.findElement(By.xpath("./..//input")).sendKeys(value);
-        Actions action = new Actions(driver);
-        action.moveToElement(webElement)
-                .click()
-                .sendKeys(Keys.ENTER)
-                .perform();
-    }
-
-    public void chooseFirstResult(Data value) {
-        DelayUtils.waitByXPath(webDriverWait, searchFirstResultXpath(value.getStringValue()));
-        webElement.findElement(By.xpath(searchFirstResultXpath(value.getStringValue()))).click();
-    }
-
+    
     @Override
     public void setValueContains(Data value) {
         webElement.click();
-        webElement.findElement(By.xpath(INPUT)).sendKeys(value.getStringValue());
+        webElement.findElement(By.xpath(INPUT_XPATH)).sendKeys(value.getStringValue());
         DelayUtils.waitForPageToLoad(driver, webDriverWait);
         DropdownList dropdownList = DropdownList.create(driver, webDriverWait);
         dropdownList.selectOptionContains(value.getStringValue());
     }
-
+    
     @Override
     public Data getValue() {
-        return Data.createSingleData(webElement.findElement(By.xpath(INPUT)).getAttribute("value"));
+        return Data.createSingleData(webElement.findElement(By.xpath(INPUT_XPATH)).getAttribute("value"));
     }
-
+    
     @Override
     public void setValue(Data value) {
         DelayUtils.waitForClickability(webDriverWait, webElement);
         webElement.click();
-        DelayUtils.sleep();//wait for cursor
+        DelayUtils.sleep();// wait for cursor
         clear();
-        webElement.findElement(By.xpath(INPUT)).sendKeys(value.getStringValue());
+        webElement.findElement(By.xpath(INPUT_XPATH)).sendKeys(value.getStringValue());
         DelayUtils.waitForSpinners(webDriverWait, webElement);
-        chooseFirstResult(value);
+        DropdownList.create(driver, webDriverWait).selectOption(value.getStringValue());
     }
-
+    
     @Override
     public void clear() {
-        WebElement input = webElement.findElement(By.xpath(INPUT));
+        WebElement input = webElement.findElement(By.xpath(INPUT_XPATH));
         input.sendKeys(Keys.CONTROL + "a");
         input.sendKeys(Keys.DELETE);
     }
-
+    
     @Override
     public String getLabel() {
         return webElement.findElement(By.tagName("span")).getText();
     }
-
-    private String searchFirstResultXpath(String value) {
-        return "(//div[@class='ExtendedSearchComponent']//div[contains(@class,'CustomSelectList-data')][./*[contains(text(), '" + value + "')]])[1] | (//div[contains(@class, 'list-item')])[1]";
-    }
+    
 }
