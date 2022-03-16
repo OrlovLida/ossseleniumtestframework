419679149900e70fd900c938e0596133c0ade407
Faustyna.Szczepanik <faustyna.szczepanik@comarch.com>
2022-03-16 10:28:02 +0100

    ER OSSWEB-17091

--- src/main/java/com/oss/framework/widgets/tabs/TabsWidget.java
+++ src/main/java/com/oss/framework/widgets/tabs/TabsWidget.java
@@ -40,6 +40,7 @@ public class TabsWidget extends Widget implements TabsInterface {
     private static final String TAB_BY_ID_PATTERN = ".//a[@id='%s']";
     private static final String ACTIVE_TAB_CONTENT = ".//div[@data-testid='%s']//div[contains(@class,'tabsContainerSingleContent active')]";
     private static final String REMOVE_TAB_XPATH = ".//*[@title='Remove tab']";
+    private static final String ANCESTOR_XPATH = "//ancestor::a";
 
     private TabsWidget(WebDriver driver, WebDriverWait wait, String id) {
         super(driver, wait, id);
@@ -201,9 +202,9 @@ public class TabsWidget extends Widget implements TabsInterface {
             webDriverWait.until(ExpectedConditions.elementToBeClickable(moreTab));
             moreTab.click();
             DelayUtils.waitByXPath(webDriverWait, xPathForTab);
-            return driver.findElement(By.xpath(xPathForTab + "//ancestor::a"));
+            return driver.findElement(By.xpath(xPathForTab + ANCESTOR_XPATH));
         } else {
-            return createTabs().findElement(By.xpath(xPathForTab + "//ancestor::a"));
+            return createTabs().findElement(By.xpath(xPathForTab + ANCESTOR_XPATH));
         }
     }
 }
