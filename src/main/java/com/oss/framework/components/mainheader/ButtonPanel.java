c314a61295e0e17075a3dcd0e86574e86ba537f4
Faustyna.Szczepanik <faustyna.szczepanik@comarch.com>
2022-03-31 13:24:18 +0200

    ER OSSWEB-17476

--- src/main/java/com/oss/framework/components/mainheader/ButtonPanel.java
+++ src/main/java/com/oss/framework/components/mainheader/ButtonPanel.java
@@ -13,11 +13,14 @@ public class ButtonPanel {
 
     private static final String BUTTON_PANEL_XPATH = "//div[@class='view-actions-container']";
     private static final String ICON_XPATH_PATTERN = ".//*[contains(@" + CSSUtils.TEST_ID + ",'%s')]";
-    private static final String SAVE_CONFIGURATION_ICON_ID = "fa fa-fw fa-floppy-o";
-    private static final String CHOOSE_CONFIGURATION_ICON_ID = "fa fa-fw fa-cog";
-    private static final String DOWNLOAD_CONFIGURATION_ICON_ID = "fa fa-fw fa-download";
     private static final String BUTTON_PANEL_CSS = "div.view-actions-container";
-    private static final String HORIZONTAL_LAYOUT_BUTTON_CLASS = "ossfont-layout-horizontal";
+    private static final String HORIZONTAL_50_50_LAYOUT_BUTTON_CLASS = "ossfont-layout-horizontal";
+    private static final String HORIZONTAL_40_60_LAYOUT_BUTTON_CLASS = "ossfont-layout-horizontal-40-60";
+    private static final String HORIZONTAL_60_40_LAYOUT_BUTTON_CLASS = "ossfont-layout-horizontal-60-40";
+    private static final String HORIZONTAL_60_40_BUTTON_ID = "TWO_ROWS_60_40";
+    private static final String HORIZONTAL_40_60_BUTTON_ID = "TWO_ROWS_40_60";
+    private static final String HORIZONTAL_50_50_BUTTON_ID = "TWO_ROWS_50_50";
+    private static final String VERTICAL_LAYOUT_BUTTON_CLASS = "ossfont-layout-vertical";
 
     private final WebDriver driver;
     private final WebDriverWait wait;
@@ -43,8 +46,32 @@ public class ButtonPanel {
         DropdownList.create(driver, wait).selectOptionById(actionId);
     }
 
-    public boolean isHorizontalLayout() {
-        return !webElement.findElements(By.className(HORIZONTAL_LAYOUT_BUTTON_CLASS)).isEmpty();
+    public boolean isHorizontalLayout(String layoutId) {
+        return !webElement.findElements(By.className(getHorizontalLayoutType(layoutId))).isEmpty();
+    }
+
+    private String getHorizontalLayoutType(String layoutId) {
+        String layoutClass;
+        switch (layoutId) {
+            case HORIZONTAL_60_40_BUTTON_ID:
+                layoutClass = HORIZONTAL_60_40_LAYOUT_BUTTON_CLASS;
+                break;
+            case HORIZONTAL_50_50_BUTTON_ID:
+                layoutClass = HORIZONTAL_50_50_LAYOUT_BUTTON_CLASS;
+                break;
+            case HORIZONTAL_40_60_BUTTON_ID:
+                layoutClass = HORIZONTAL_40_60_LAYOUT_BUTTON_CLASS;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + layoutId);
+        }
+
+        return layoutClass;
+
+    }
+
+    public boolean isVerticalLayout() {
+        return !webElement.findElements(By.className(VERTICAL_LAYOUT_BUTTON_CLASS)).isEmpty();
     }
 
     private WebElement getButtonPanel() {
