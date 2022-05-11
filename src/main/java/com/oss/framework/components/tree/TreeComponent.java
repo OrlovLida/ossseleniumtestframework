ceced746f86561b83dc20e83591e87cb57f2e47b
Gabriela Kasza <gabriela.kasza@comarch.com>
2022-05-11 16:35:59 +0200

    ER OSSWEB-17515 # move to Node added

--- src/main/java/com/oss/framework/components/tree/TreeComponent.java
+++ src/main/java/com/oss/framework/components/tree/TreeComponent.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
