bb5f4ba2f071e087d96121816c1177e597c6bc18
Agnieszka.Podolska <agnieszka.podolska@comarch.com>
2022-05-17 11:19:11 +0200

    poprawki po CR

--- src/main/java/com/oss/framework/components/layout/Card.java
+++ src/main/java/com/oss/framework/components/layout/Card.java
@@ -7,9 +7,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.oss.framework.components.contextactions.ActionsContainer;
+import com.oss.framework.components.contextactions.ActionsInterface;
+import com.oss.framework.components.contextactions.ButtonContainer;
 import com.oss.framework.components.contextactions.OldActionsContainer;
 import com.oss.framework.utils.CSSUtils;
 import com.oss.framework.utils.DelayUtils;
+import com.oss.framework.utils.WebElementUtils;
 
 public class Card {
 
@@ -17,6 +21,10 @@ public class Card {
     private static final String MAXIMIZE_CHART_BUTTON_XPATH = ".//*[contains(@" + CSSUtils.TEST_ID + ", 'maximize')]";
     private static final String MINIMIZE_CHART_BUTTON_XPATH = ".//*[contains(@" + CSSUtils.TEST_ID + ", 'minimize')]";
     private static final String CARD_CONTAINER_NAME_PATTERN = ".//div[contains(@class, 'simple-card-container')]//*[contains(text(), '%s')]";
+    private static final String ACTIONS_CONTAINER_CSS = ".actionsContainer";
+    private static final String WINDOW_TOOLBAR_CSS = ".windowToolbar";
+    private static final String CONTEXT_ACTIONS_CSS = WINDOW_TOOLBAR_CSS + "," + ACTIONS_CONTAINER_CSS;
+
     private final WebDriver driver;
     private final WebDriverWait wait;
     private final WebElement cardElement;
@@ -57,15 +65,29 @@ public class Card {
         return !cardElement.findElements(By.xpath(MINIMIZE_CHART_BUTTON_XPATH)).isEmpty();
     }
 
-    public void clickContextAction(String actionLabel) {
-        getOldActionsContainer().callActionByLabel(actionLabel);
+    public void callAction(String actionLabel) {
+        getActionsInterface().callActionByLabel(actionLabel);
     }
 
-    public void clickContextActionById(String actionId) {
-        getOldActionsContainer().callActionById(actionId);
+    public void callActionById(String actionId) {
+        getActionsInterface().callActionById(actionId);
     }
 
-    private OldActionsContainer getOldActionsContainer() {
-        return OldActionsContainer.createFromParent(driver, wait, cardElement);
+    private ActionsInterface getActionsInterface() {
+        DelayUtils.waitForPageToLoad(driver, wait);
+        DelayUtils.waitBy(wait, By.cssSelector(CONTEXT_ACTIONS_CSS));
+        boolean isNewActionContainer = isElementPresent(cardElement, By.cssSelector(ACTIONS_CONTAINER_CSS));
+        boolean isOldActionContainer = isElementPresent(cardElement, By.cssSelector(WINDOW_TOOLBAR_CSS));
+        if (isNewActionContainer) {
+            return ActionsContainer.createFromParent(cardElement, driver, wait);
+        } else if (isOldActionContainer)
+            return OldActionsContainer.createFromParent(driver, wait, cardElement);
+        else {
+            return ButtonContainer.createFromParent(cardElement, driver, wait);
+        }
+    }
+
+    private boolean isElementPresent(WebElement webElement, By by) {
+        return WebElementUtils.isElementPresent(webElement, by);
     }
 }
