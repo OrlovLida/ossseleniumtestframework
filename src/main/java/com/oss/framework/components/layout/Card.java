6a3da51772bc933989ecf00ddfe65476fe722ddd
Agnieszka.Podolska <agnieszka.podolska@comarch.com>
2022-05-17 10:08:38 +0200

    tworzenie card po nazwie i dodanie context actions

--- src/main/java/com/oss/framework/components/layout/Card.java
+++ src/main/java/com/oss/framework/components/layout/Card.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.oss.framework.components.contextactions.OldActionsContainer;
 import com.oss.framework.utils.CSSUtils;
 import com.oss.framework.utils.DelayUtils;
 
@@ -15,6 +16,7 @@ public class Card {
     private static final Logger log = LoggerFactory.getLogger(Card.class);
     private static final String MAXIMIZE_CHART_BUTTON_XPATH = ".//*[contains(@" + CSSUtils.TEST_ID + ", 'maximize')]";
     private static final String MINIMIZE_CHART_BUTTON_XPATH = ".//*[contains(@" + CSSUtils.TEST_ID + ", 'minimize')]";
+    private static final String CARD_CONTAINER_NAME_PATTERN = ".//div[contains(@class, 'simple-card-container')]//*[contains(text(), '%s')]";
     private final WebDriver driver;
     private final WebDriverWait wait;
     private final WebElement cardElement;
@@ -28,6 +30,12 @@ public class Card {
     public static Card createCard(WebDriver driver, WebDriverWait wait, String windowId) {
         DelayUtils.waitByXPath(wait, ".//div[contains(@" + CSSUtils.TEST_ID + ", '" + windowId + "')]");
         WebElement card = driver.findElement(By.xpath(".//div[@" + CSSUtils.TEST_ID + "='" + windowId + "']"));
+        return new Card(driver, wait, card);
+    }
+
+    public static Card createCardByName(WebDriver driver, WebDriverWait wait, String cardName) {
+        DelayUtils.waitByXPath(wait, String.format(CARD_CONTAINER_NAME_PATTERN, cardName));
+        WebElement card = driver.findElement(By.xpath(String.format(CARD_CONTAINER_NAME_PATTERN, cardName)));
         return new Card(driver, wait, card);
     }
 
@@ -47,5 +55,17 @@ public class Card {
 
     public boolean isCardMaximized() {
         return !cardElement.findElements(By.xpath(MINIMIZE_CHART_BUTTON_XPATH)).isEmpty();
+    }
+
+    public void clickContextAction(String actionLabel) {
+        getOldActionsContainer().callActionByLabel(actionLabel);
+    }
+
+    public void clickContextActionById(String actionId) {
+        getOldActionsContainer().callActionById(actionId);
+    }
+
+    private OldActionsContainer getOldActionsContainer() {
+        return OldActionsContainer.createFromParent(driver, wait, cardElement);
     }
 }
