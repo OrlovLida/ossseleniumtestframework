47a1bd027004d5971316ab8069f234bfcf598c01
Krzysztof Pogonowski <krzysztof.pogonowski@comarch.com>
2022-05-30 18:48:47 +0000

    Update DelayUtils.java

--- src/main/java/com/oss/framework/utils/DelayUtils.java
+++ src/main/java/com/oss/framework/utils/DelayUtils.java
@@ -81,7 +81,11 @@ public class DelayUtils {
     }
 
     public static void waitForElementDisappear(WebDriverWait wait, WebElement webElement) {
-        wait.ignoring(StaleElementReferenceException.class, TimeoutException.class).until(ExpectedConditions.invisibilityOf(webElement));
+        try {
+            wait.ignoring(StaleElementReferenceException.class).until(ExpectedConditions.invisibilityOf(webElement));
+        } catch (TimeoutException e) {
+            log.error("Some element(s) not disappear in expected time");
+        }
     }
 
     public static void waitForVisibility(WebDriverWait wait, List<WebElement> webElements) {
