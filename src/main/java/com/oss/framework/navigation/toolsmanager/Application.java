5b20463948f54ab4d07cd73c2fa26609c4cebc6b
Gabriela Kasza <gabriela.kasza@comarch.com>
2022-07-04 16:48:14 +0200

    ER OSSWEB-18971 # refactor

--- src/main/java/com/oss/framework/navigation/toolsmanager/Application.java
+++ src/main/java/com/oss/framework/navigation/toolsmanager/Application.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.oss.framework.components.contextactions.InlineMenu;
@@ -38,34 +37,35 @@ public class Application {
     private static final String TEXT_CONTENT_ATTRIBUTE = "textContent";
     private static final String HREF_ATTRIBUTE = "href";
     private static final String APPLICATION_LOADER = ".category-box .skeleton-preloader";
-
+    
     private WebDriver driver;
     private WebDriverWait wait;
     private WebElement applicationBox;
     private String applicationName;
-
+    
     private Application(WebDriver driver, WebDriverWait wait, WebElement applicationBox, String applicationName) {
         this.driver = driver;
         this.wait = wait;
         this.applicationBox = applicationBox;
         this.applicationName = applicationName;
     }
-
+    
     static Application createApplicationByName(WebDriver driver, WebDriverWait wait, WebElement parent, String applicationName) {
         DelayUtils.waitForNumberOfElementsToBe(wait, By.cssSelector(APPLICATION_LOADER), 0);
         WebElement applicationBox = parent.findElements(By.cssSelector(APPLICATIONS_IN_SUBCATEGORY_CSS)).stream()
-                .filter(application -> application.findElement(By.cssSelector(APPLICATION_BOX_CSS)).getAttribute(TEXT_CONTENT_ATTRIBUTE).equals(applicationName))
+                .filter(application -> application.findElement(By.cssSelector(APPLICATION_BOX_CSS)).getAttribute(TEXT_CONTENT_ATTRIBUTE)
+                        .equals(applicationName))
                 .findFirst().orElseThrow(() -> new NoSuchElementException(CANNOT_FIND_APPLICATION_BOX_WITH_PROVIDED_NAME_EXCEPTION));
-                WebElementUtils.moveToElement(driver, applicationBox);
+        WebElementUtils.moveToElement(driver, applicationBox);
         return new Application(driver, wait, applicationBox, applicationName);
     }
-
+    
     static Application createApplication(WebDriver driver, WebDriverWait wait, WebElement applicationBox) {
         DelayUtils.waitForNumberOfElementsToBe(wait, By.cssSelector(APPLICATION_LOADER), 0);
         String applicationName = applicationBox.getAttribute(TEXT_CONTENT_ATTRIBUTE);
         return new Application(driver, wait, applicationBox, applicationName);
     }
-
+    
     public Optional<String> getApplicationsURL() {
         List<WebElement> links = applicationBox.findElements(By.cssSelector(APPLICATION_LINK_CSS));
         if (!links.isEmpty()) {
@@ -73,40 +73,40 @@ public class Application {
         } else
             return Optional.empty();
     }
-
+    
     public String getApplicationName() {
         return applicationName;
     }
-
+    
     public boolean isFavorite() {
         return getStar().getAttribute(ARIA_LABEL_ATTRIBUTE).equals(FAVOURITE);
     }
-
+    
     public void markFavorite() {
         if (!isFavorite()) {
             WebElementUtils.clickWebElement(driver, getStar());
         }
     }
-
+    
     void openApplication() {
         applicationBox.findElement(By.className(APPLICATION_LINK_CLASS)).click();
     }
-
+    
     void callAction(String actionId) {
         InlineMenu.create(applicationBox, driver, wait).callAction(actionId);
     }
-
+    
     DragAndDrop.DropElement getDropElement() {
         return new DragAndDrop.DropElement(applicationBox);
     }
-
+    
     DragAndDrop.DraggableElement getDragElement() {
         WebElement dragButton = applicationBox.findElement(By.xpath(DRAG_BUTTON_XPATH));
         return new DragAndDrop.DraggableElement(dragButton);
     }
-
+    
     private WebElement getStar() {
         return applicationBox.findElement(By.cssSelector(OSSICON_CSS));
     }
-
+    
 }
