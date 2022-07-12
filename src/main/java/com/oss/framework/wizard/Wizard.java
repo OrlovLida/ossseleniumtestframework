54d5600126a88a02a5266892b73bfaf8169e2fe9
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2022-07-12 14:35:07 +0200

    ER-OSSWEB-19144 # add getButtonLabel method to Wizard

--- src/main/java/com/oss/framework/wizard/Wizard.java
+++ src/main/java/com/oss/framework/wizard/Wizard.java
@@ -22,7 +22,7 @@ import com.oss.framework.utils.WebElementUtils;
 import com.oss.framework.widgets.Widget;
 
 public class Wizard {
-    
+
     private static final String OSS_WINDOW_XPATH = "//div[contains(@class,'OssWindow')]";
     private static final String NEXT_BUTTON_XPATH = ".//button[text()='Next']";
     private static final String NEXT_STEP_XPATH = ".//button[text()='Next Step']";
@@ -36,11 +36,12 @@ public class Wizard {
     private static final String BY_DATA_TEST_ID_PATTERN = "//*[@" + CSSUtils.TEST_ID + "='%s']";
     private static final String WIZARD_STEPS_CSS = ".simple-progress-bar-step";
     private static final String ACTIVE_STEP_CSS = ".simple-progress-bar-step--active";
+    private static final String TEXT_CONTENT_ATTRIBUTE = "textContent";
     private final WebDriver driver;
     private final WebDriverWait wait;
     private final WebElement webElement;
     private final String wizardId;
-    
+
     private Wizard(WebDriver driver, WebDriverWait wait, WebElement webElement, String wizardId) {
         this.driver = driver;
         this.wait = wait;
@@ -49,7 +50,7 @@ public class Wizard {
     }
 
     /**
-     * @deprecated (to remove with next release 3.0.x, please use method createByComponentId(WebDriver driver, WebDriverWait wait, String wizardId))
+     * @deprecated (to remove with next release 3.0.x, please use method createByComponentId ( WebDriver driver, WebDriverWait wait, String wizardId))
      */
     @Deprecated
     public static Wizard createWizard(WebDriver driver, WebDriverWait wait) {
@@ -58,51 +59,51 @@ public class Wizard {
         String wizardId = CSSUtils.getAttributeValue(CSSUtils.TEST_ID, webElement);
         return new Wizard(driver, wait, webElement, wizardId);
     }
-    
+
     public static Wizard createByComponentId(WebDriver driver, WebDriverWait wait, String wizardId) {
         Widget.waitForWidgetById(wait, wizardId);
         WebElement webElement = driver.findElement(By.xpath(String.format(BY_DATA_TEST_ID_PATTERN, wizardId)));
         return new Wizard(driver, wait, webElement, wizardId);
     }
-    
+
     private static boolean isElementPresent(WebElement webElement, By by) {
         return WebElementUtils.isElementPresent(webElement, by);
     }
-    
+
     public Input getComponent(String componentId, Input.ComponentType componentType) {
         return ComponentFactory.createFromParent(componentId, componentType, this.driver, this.wait, this.webElement);
     }
-    
+
     public Input getComponent(String componentId) {
         return ComponentFactory.createFromParent(componentId, this.driver, this.wait, this.webElement);
     }
-    
+
     public Input setComponentValue(String componentId, String value, Input.ComponentType componentType) {
         DelayUtils.waitForNestedElements(wait, webElement, String.format(BY_DATA_TEST_ID_PATTERN, componentId));
         Input input = getComponent(componentId, componentType);
         input.setSingleStringValue(value);
         return input;
     }
-    
+
     public Input setComponentValue(String componentId, String value) {
         DelayUtils.waitForNestedElements(wait, webElement, String.format(BY_DATA_TEST_ID_PATTERN, componentId));
         Input input = getComponent(componentId);
         input.setSingleStringValue(value);
         return input;
     }
-    
+
     public void clickNext() {
         clickButton(NEXT_BUTTON_XPATH);
     }
-    
+
     public void clickNextStep() {
         clickButton(NEXT_STEP_XPATH);
     }
-    
+
     public void clickAccept() {
         clickButton(ACCEPT_BUTTON_XPATH);
     }
-    
+
     public void clickAcceptOldWizard() {
         DelayUtils.waitForNestedElements(wait, webElement, ACCEPT_BUTTON_XPATH);
         WebElement button = webElement.findElement(By.xpath(ACCEPT_BUTTON_XPATH));
@@ -114,51 +115,51 @@ public class Wizard {
         DelayUtils.sleep();
         actions.release().perform();
     }
-    
+
     public void clickCancel() {
         clickButton(CANCEL_BUTTON_XPATH);
     }
-    
+
     public void clickProceed() {
         clickButton(PROCEED_BUTTON_XPATH);
     }
-    
+
     public void clickSave() {
         clickButton(SAVE_BUTTON_XPATH);
     }
-    
+
     public void clickUpdate() {
         clickButton(UPDATE_BUTTON_XPATH);
     }
-    
+
     public void clickOK() {
         clickButton(OK_BUTTON_XPATH);
     }
-    
+
     public void clickButtonById(String actionId) {
         clickButton(String.format(BY_DATA_TEST_ID_PATTERN, actionId));
     }
-    
+
     public void clickButtonByLabel(String label) {
         clickButton(String.format(BY_TEXT_PATTERN, label));
     }
-    
+
     public void clickButtonById(String groupId, String actionId) {
         ActionsInterface buttonContainer = ButtonContainer.createFromParent(webElement, driver, wait);
         buttonContainer.callActionById(groupId, actionId);
     }
-    
+
     public void waitToClose() {
         wait.until(ExpectedConditions.invisibilityOf(this.webElement));
     }
-    
+
     public void rolloutById(String id) {
         if (isElementPresent(webElement,
                 By.xpath("//div[contains(@class, 'collapsedRollout')]/div[@" + CSSUtils.TEST_ID + "='" + id + "']"))) {
             webElement.findElement(By.xpath(String.format(BY_DATA_TEST_ID_PATTERN, id) + "/span")).click();
         }
     }
-    
+
     public void skipAndAccept(String acceptButtonId) {
         int stepsNumber = countNumberOfSteps();
         if (stepsNumber > 1) {
@@ -169,67 +170,71 @@ public class Wizard {
         }
         clickButtonById(acceptButtonId);
     }
-    
+
     public int countNumberOfSteps() {
         if (isStepsPresent()) {
             return getWizardSteps().size();
         }
         return 1;
     }
-    
+
     public TreeComponent getTreeComponent() {
         return TreeComponent.create(driver, wait, webElement);
     }
-    
+
     public TableComponent getTableComponent() {
         return TableComponent.create(driver, wait, wizardId);
     }
-    
+
     public TableComponent getTableComponent(String tableComponentId) {
         return TableComponent.createById(driver, wait, tableComponentId);
     }
-    
+
     private void clickButton(String xpath) {
         DelayUtils.waitForNestedElements(wait, webElement, xpath);
         WebElement button = webElement.findElement(By.xpath(xpath));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", button);
-        Actions actions = new Actions(driver);
-        actions.moveToElement(button).build().perform();
-        wait.until(ExpectedConditions.elementToBeClickable(button));
-        actions.click(button).build().perform();
+        WebElementUtils.clickWebElement(driver, button);
     }
-    
+
+    public String getButtonLabel(String id) {
+        String xpath = String.format(BY_DATA_TEST_ID_PATTERN, id);
+        DelayUtils.waitForNestedElements(wait, webElement, xpath);
+        WebElement button = webElement.findElement(By.xpath(xpath));
+        WebElementUtils.moveToElement(driver, button);
+        return button.getAttribute(TEXT_CONTENT_ATTRIBUTE);
+    }
+
     private boolean isStepsPresent() {
         List<WebElement> steps = this.webElement.findElements(By.xpath("//div[@class='simple-progress-bar']"));
         return !steps.isEmpty();
     }
-    
+
     private List<WebElement> getWizardSteps() {
         DelayUtils.waitForNestedElements(wait, webElement, By.cssSelector(WIZARD_STEPS_CSS));
         return this.webElement.findElements(By.cssSelector(WIZARD_STEPS_CSS));
     }
-    
+
     private WebElement getCurrentStep() {
         DelayUtils.waitForNestedElements(wait, webElement, By.cssSelector(WIZARD_STEPS_CSS));
         return this.webElement.findElement(By.cssSelector(ACTIVE_STEP_CSS));
     }
-    
+
     public String getCurrentStepTitle() {
         return getCurrentStep().getText();
     }
-    
+
     public boolean isNextStepPresent() {
         List<WebElement> steps = getWizardSteps();
         WebElement currentStep = getCurrentStep();
         return (steps.size() > steps.indexOf(currentStep) + 1);
     }
-    
+
     public boolean isElementPresentById(String id) {
         return isElementPresent(webElement, By.xpath(".//*[@" + CSSUtils.TEST_ID + "='" + id + "']"));
     }
-    
+
     public void waitForWizardToLoad() {
         DelayUtils.waitForElementToLoad(wait, webElement);
     }
-    
+
 }
