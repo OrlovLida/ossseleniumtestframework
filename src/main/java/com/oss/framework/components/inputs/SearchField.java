6f8a239b185a57fcf370a4687b7e98735eb89acd
NBKPOGONOWSKI <krzysztof.pogonowski@comarch.pl>
2022-07-22 16:08:04 +0200

    ER-OSSTEST-121386 # E2E tests update

--- src/main/java/com/oss/framework/components/inputs/SearchField.java
+++ src/main/java/com/oss/framework/components/inputs/SearchField.java
@@ -13,60 +13,60 @@ import com.oss.framework.utils.DelayUtils;
 import com.oss.framework.utils.WebElementUtils;
 
 public class SearchField extends Input {
-    
+
     private static final String INPUT_XPATH = ".//input";
-    
+
     private SearchField(WebDriver driver, WebDriverWait wait, WebElement webElement, String componentId) {
         super(driver, wait, webElement, componentId);
     }
-    
+
     static SearchField create(WebDriver driver, WebDriverWait wait, String componentId) {
         WebElement webElement = driver.findElement(By.cssSelector(CSSUtils.getElementCssSelector(componentId)));
         WebElementUtils.moveToElement(driver, webElement);
         return new SearchField(driver, wait, webElement, componentId);
     }
-    
+
     static SearchField createFromParent(WebElement parent, WebDriver driver, WebDriverWait wait, String componentId) {
         WebElement webElement = parent.findElement(By.cssSelector(CSSUtils.getElementCssSelector(componentId)));
         WebElementUtils.moveToElement(driver, webElement);
         return new SearchField(driver, wait, webElement, componentId);
     }
-    
+
     @Override
     public void setValueContains(Data value) {
-        webElement.click();
-        webElement.findElement(By.xpath(INPUT_XPATH)).sendKeys(value.getStringValue());
-        DelayUtils.waitForPageToLoad(driver, webDriverWait);
-        DropdownList dropdownList = DropdownList.create(driver, webDriverWait);
-        dropdownList.selectOptionContains(value.getStringValue());
+        typeValue(value);
+        DropdownList.create(driver, webDriverWait).selectOptionContains(value.getStringValue());
     }
-    
+
     @Override
     public Data getValue() {
         return Data.createSingleData(webElement.findElement(By.xpath(INPUT_XPATH)).getAttribute("value"));
     }
-    
+
     @Override
     public void setValue(Data value) {
-        DelayUtils.waitForClickability(webDriverWait, webElement);
-        webElement.click();
-        DelayUtils.sleep();// wait for cursor
-        clear();
-        webElement.findElement(By.xpath(INPUT_XPATH)).sendKeys(value.getStringValue());
-        DelayUtils.waitForSpinners(webDriverWait, webElement);
+        typeValue(value);
         DropdownList.create(driver, webDriverWait).selectOption(value.getStringValue());
     }
-    
+
     @Override
     public void clear() {
         WebElement input = webElement.findElement(By.xpath(INPUT_XPATH));
         input.sendKeys(Keys.CONTROL + "a");
         input.sendKeys(Keys.DELETE);
     }
-    
+
     @Override
     public String getLabel() {
         return webElement.findElement(By.tagName("span")).getText();
     }
-    
+
+    private void typeValue(Data value) {
+        DelayUtils.waitForClickability(webDriverWait, webElement);
+        webElement.click();
+        DelayUtils.sleep();// wait for cursor
+        clear();
+        webElement.findElement(By.xpath(INPUT_XPATH)).sendKeys(value.getStringValue());
+        DelayUtils.waitForSpinners(webDriverWait, webElement);
+    }
 }
