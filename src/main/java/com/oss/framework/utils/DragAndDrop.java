68e41902b8a278d866463f29e2b3d9254ec67add
Gabriela Kasza <gabriela.kasza@comarch.com>
2022-07-29 13:36:08 +0200

    ER OSSWEB-19465 # wait added after drag&drop

--- src/main/java/com/oss/framework/utils/DragAndDrop.java
+++ src/main/java/com/oss/framework/utils/DragAndDrop.java
@@ -4,13 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DragAndDrop {
-
+    
     private DragAndDrop() {
         throw new IllegalStateException("Utility class");
     }
-
+    
     public static void dragAndDrop(String xpathToSource, String xpathToTarget, WebDriver driver) {
         WebElement source = driver.findElement(By.xpath(xpathToSource));
         WebElement target = driver.findElement(By.xpath(xpathToTarget));
@@ -18,7 +19,7 @@ public class DragAndDrop {
         action.moveToElement(source).pause(100).clickAndHold(source).pause(100).moveByOffset(10, 10).moveToElement(target)
                 .moveByOffset(1, 0).pause(100).release().perform();
     }
-
+    
     public static void dragAndDrop(String xpathToSource, String xpathToTarget, int pauseInMs, WebDriver driver) {
         WebElement source = driver.findElement(By.xpath(xpathToSource));
         WebElement target = driver.findElement(By.xpath(xpathToTarget));
@@ -26,49 +27,57 @@ public class DragAndDrop {
         action.moveToElement(source).pause(pauseInMs).clickAndHold(source).pause(pauseInMs).moveByOffset(10, 10).moveToElement(target)
                 .moveByOffset(1, 0).pause(pauseInMs).release().perform();
     }
-
+    
     public static void dragAndDrop(DraggableElement source, DropElement target, WebDriver driver) {
         Actions action = new Actions(driver);
         action.moveToElement(source.getWebElement()).pause(100).clickAndHold(source.getWebElement()).pause(100).moveByOffset(10, 10)
                 .moveToElement(target.getWebElement())
                 .moveByOffset(1, 0).pause(100).release().perform();
     }
-
+    
     public static void dragAndDrop(DraggableElement source, DropElement target, int xOffset, int yOffset, WebDriver driver) {
         Actions action = new Actions(driver);
         action.moveToElement(source.getWebElement()).pause(100).clickAndHold(source.getWebElement()).pause(100).moveByOffset(10, 10)
                 .moveToElement(target.getWebElement())
                 .moveByOffset(xOffset, yOffset).pause(100).release().perform();
     }
-
+    
     public static void dragAndDrop(WebElement source, WebElement target, int pauseInMs, WebDriver driver) {
         Actions action = new Actions(driver);
         action.moveToElement(source).pause(pauseInMs).clickAndHold(source).pause(pauseInMs).moveByOffset(10, 10).moveToElement(target)
                 .moveByOffset(1, 0).pause(pauseInMs).release().perform();
     }
-
+    
     public static class DraggableElement {
         private final WebElement webElement;
-
+        
         public DraggableElement(WebElement webElement) {
             this.webElement = webElement;
         }
-
+        
         private WebElement getWebElement() {
             return webElement;
         }
+        
+        public void waitUntilElementRecalculate(WebDriverWait wait) {
+            DelayUtils.waitForElementDisappear(wait, webElement);
+        }
     }
-
+    
     public static class DropElement {
         private final WebElement webElement;
-
+        
         public DropElement(WebElement webElement) {
             this.webElement = webElement;
         }
-
+        
+        public void waitUntilElementRecalculate(WebDriverWait wait) {
+            DelayUtils.waitForElementDisappear(wait, webElement);
+        }
+        
         private WebElement getWebElement() {
             return webElement;
         }
     }
-
+    
 }
