a2c85ee37ca2977b9f0f0f16856afe82c14af8f9
Paweł Rother <pawel.rother@comarch.com>
2022-12-20 09:32:07 +0100

    OSSWEB-21684
    dodanie metody getMessageLinkURL do MessageContainer

--- src/main/java/com/oss/framework/components/alerts/SystemMessageInterface.java
+++ src/main/java/com/oss/framework/components/alerts/SystemMessageInterface.java
@@ -1,20 +1,22 @@
 package com.oss.framework.components.alerts;
 
+import com.oss.framework.components.alerts.SystemMessageContainer.Message;
+
 import java.util.List;
 import java.util.Optional;
 
-import com.oss.framework.components.alerts.SystemMessageContainer.Message;
-
 public interface SystemMessageInterface {
-    
+
     List<Message> getMessages();
-    
+
     Optional<Message> getFirstMessage();
-    
+
     void close();
-    
+
     void clickMessageLink();
-    
+
+    Optional<String> getMessageLinkURL();
+
     void waitForMessageDisappear();
 
 }
